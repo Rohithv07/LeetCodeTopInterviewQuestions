ef28e8bc3d2d0dec87189b0f388dc82fce6de3d0
vazha <rohithv63@gmail.com>
2020-07-23 00:26:56 +0530

    Possible sums code signal solution

---
+++ possibleSums.py
@@ -0,0 +1,33 @@
+You have a collection of coins, and you know the values of the coins and the quantity of each type of coin in it. You want to know how many distinct sums you can make from non-empty groupings of these coins.
+
+Example
+
+For coins = [10, 50, 100] and quantity = [1, 2, 1], the output should be
+possibleSums(coins, quantity) = 9.
+
+Here are all the possible sums:
+
+50 = 50;
+10 + 50 = 60;
+50 + 100 = 150;
+10 + 50 + 100 = 160;
+50 + 50 = 100;
+10 + 50 + 50 = 110;
+50 + 50 + 100 = 200;
+10 + 50 + 50 + 100 = 210;
+10 = 10;
+100 = 100;
+10 + 100 = 110.
+As you can see, there are 9 distinct sums that can be created from non-empty groupings of your coins.
+
+Solutions:
+
+def possibleSums(coins, quantity):
+    d = {0}
+    
+    for c, q in zip(coins, quantity):
+        d = {x + c * i for x in d for i in range(q + 1)}
+        
+    return len(d) - 1
+        
+
