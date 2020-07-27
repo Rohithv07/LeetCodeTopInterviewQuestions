80120b3f77cfabefd13c6df9302ea9e53383799f
vazha <rohithv63@gmail.com>
2020-07-27 11:53:31 +0530

    Reversing the linked in list in given k order

---
+++ reverseListInKOrder.py
@@ -0,0 +1,53 @@
+Note: Your solution should have O(n) time complexity, where n is the number of elements in l, and O(1) additional space complexity, since this is what you would be asked to accomplish in an interview.
+
+Given a linked list l, reverse its nodes k at a time and return the modified list. k is a positive integer that is less than or equal to the length of l. If the number of nodes in the linked list is not a multiple of k, then the nodes that are left out at the end should remain as-is.
+
+You may not alter the values in the nodes - only the nodes themselves can be changed.
+
+Example
+
+For l = [1, 2, 3, 4, 5] and k = 2, the output should be
+reverseNodesInKGroups(l, k) = [2, 1, 4, 3, 5];
+For l = [1, 2, 3, 4, 5] and k = 1, the output should be
+reverseNodesInKGroups(l, k) = [1, 2, 3, 4, 5];
+For l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] and k = 3, the output should be
+reverseNodesInKGroups(l, k) = [3, 2, 1, 6, 5, 4, 9, 8, 7, 10, 11].
+
+
+
+
+
+
+
+Solution:
+
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def reverseNodesInKGroups(l, k):
+     # Check if we need to reverse the group
+        curr = l
+        for _ in range(k):
+            if not curr: return l
+            curr = curr.next
+		        
+				
+        # Reverse the group (basic way to reverse linked list)
+        prev = None
+        curr = l
+        for _ in range(k):
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+		
+        # After reverse, we know that `head` is the tail of the group.
+		# And `curr` is the next pointer in original linked list order
+        l.next = reverseNodesInKGroups(curr, k)
+        return prev
+    
