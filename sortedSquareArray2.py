a50687a2c23a5c2d746fb11ce1848157e1ba6c2c
vazha <rohithv63@gmail.com>
2020-07-29 17:45:44 +0530

    Same solution but by using sorted()

---
+++ sortedSquareArray2.py
@@ -0,0 +1,16 @@
+You have a sorted array of integers. Write a function that returns a sorted array containing the squares of those integers.
+
+Example
+
+For array = [-6, -4, 1, 2, 3, 5], the output should be
+sortedSquaredArray(array) = [1, 4, 9, 16, 25, 36].
+
+The array of squared integers from array is: [36, 16, 1, 4, 9, 25]. When sorted, it becomes: [1, 4, 9, 16, 25, 36].
+
+SOlution:
+
+def sortedSquaredArray(array):
+    for i in range(len(array)):
+        array[i] = array[i] ** 2
+    return sorted(array)
+
