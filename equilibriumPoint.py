309e65308791083a5043bbd51973d92ba3e3c3cd
vazha <rohithv63@gmail.com>
2020-07-31 16:45:20 +0530

    Equilibrium Point implementation in Python with the idea behind solving it

---
+++ equilibriumPoint.py
@@ -0,0 +1,44 @@
+Equilibrium position in an array is a position at which the sum of elements before it is equal to the sum of elements after it. Given an array arr, your task is to determine at which position equilibrium first occurs in the array. If there is no equilibrium position, the answer should be -1.
+
+Example
+
+For arr = [5], the output should be
+equilibriumPoint(arr) = 1.
+
+Explanation: Since this array only has one element, the equilibrium point is at position 1.
+
+For arr = [10, 5, 3, 5, 2, 2, 6, 8], the output should be
+equilibriumPoint(arr) = 4.
+
+Explanation: The equilibrium point is at position 4, because the sum of elements before it - (10 + 5 + 3) - is equal to the sum of elements after it - (2 + 2 + 6 + 8).
+
+
+
+1) Initialize leftsum  as 0
+2) Get the total sum of the array as sum
+3) Iterate through the array and for each index i, do following.
+    a)  Update sum to get the right sum.  
+           sum = sum - arr[i] 
+       // sum is now right sum
+    b) If leftsum is equal to sum, then return current index. 
+       // update leftsum for next iteration.
+    c) leftsum = leftsum + arr[i]
+4) return -1 
+// If we come out of loop without returning then
+// there is no equilibrium index
+
+
+
+Solution:
+
+def equilibriumPoint(arr):
+    left_sum = 0
+    total_sum = sum(arr)
+    for i in range(len(arr)):
+        total_sum = total_sum - arr[i]
+        if left_sum == total_sum:
+            return i + 1
+        left_sum = left_sum + arr[i]
+        
+    return -1
+
