8fac468524175fe2301ab543ad896efcd9b5e5a7
rohithv07 <rohithv63@gmail.com>
2020-08-04 17:03:05 +0530

    Code signal daily challenge isSubtree()

---
+++ isSubtree.py
@@ -0,0 +1,240 @@
+Given two binary trees t1 and t2, determine whether the second tree is a subtree of the first tree. A subtree for vertex v in a binary tree t is a tree consisting of v and all its descendants in t. Determine whether or not there is a vertex v (possibly none) in tree t1 such that a subtree for vertex v (possibly empty) in t1 equals t2.
+
+Example
+
+For
+
+t1 = {
+    "value": 5,
+    "left": {
+        "value": 10,
+        "left": {
+            "value": 4,
+            "left": {
+                "value": 1,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": 2,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": {
+            "value": 6,
+            "left": null,
+            "right": {
+                "value": -1,
+                "left": null,
+                "right": null
+            }
+        }
+    },
+    "right": {
+        "value": 7,
+        "left": null,
+        "right": null
+    }
+}
+and
+
+t2 = {
+    "value": 10,
+    "left": {
+        "value": 4,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": null,
+            "right": null
+        }
+    },
+    "right": {
+        "value": 6,
+        "left": null,
+        "right": {
+            "value": -1,
+            "left": null,
+            "right": null
+        }
+    }
+}
+the output should be isSubtree(t1, t2) = true.
+
+This is what these trees look like:
+
+      t1:             t2:
+       5              10
+      / \            /  \
+    10   7          4    6
+   /  \            / \    \
+  4    6          1   2   -1
+ / \    \
+1   2   -1
+As you can see, t2 is a subtree of t1 (the vertex in t1 with value 10).
+
+For
+
+t1 = {
+    "value": 5,
+    "left": {
+        "value": 10,
+        "left": {
+            "value": 4,
+            "left": {
+                "value": 1,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": 2,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": {
+            "value": 6,
+            "left": {
+                "value": -1,
+                "left": null,
+                "right": null
+            },
+            "right": null
+        }
+    },
+    "right": {
+        "value": 7,
+        "left": null,
+        "right": null
+    }
+}
+and
+
+t2 = {
+    "value": 10,
+    "left": {
+        "value": 4,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": null,
+            "right": null
+        }
+    },
+    "right": {
+        "value": 6,
+        "left": null,
+        "right": {
+            "value": -1,
+            "left": null,
+            "right": null
+        }
+    }
+}
+the output should be isSubtree(t1, t2) = false.
+
+This is what these trees look like:
+
+        t1:            t2:
+         5             10
+       /   \          /  \
+     10     7        4    6
+   /    \           / \    \
+  4     6          1   2   -1
+ / \   / 
+1   2 -1
+As you can see, there is no vertex v such that the subtree of t1 for vertex v equals t2.
+
+For
+
+t1 = {
+    "value": 1,
+    "left": {
+        "value": 2,
+        "left": null,
+        "right": null
+    },
+    "right": {
+        "value": 2,
+        "left": null,
+        "right": null
+    }
+}
+and
+
+t2 = {
+    "value": 2,
+    "left": {
+        "value": 1,
+        "left": null,
+        "right": null
+    },
+    "right": null
+}
+the output should be isSubtree(t1, t2) = false.
+
+
+
+
+Solution:
+#
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+
+def isIdentical(root1, root2):
+    if root1 is None and root2 is None:
+        return True
+    if root1 is None or root2 is None:
+        return False
+    return(root1.value == root2.value and 
+    isIdentical(root1.left, root2.left) and
+    isIdentical(root1.right, root2.right))
+    
+def isSubtree(t1, t2):
+    if t2 is None:
+        return True
+    
+    if t1 is None:
+        return False
+    
+    if isIdentical(t1, t2):
+        return True
+        
+    return isSubtree(t1.left, t2) or isSubtree(t1.right, t2)
+
+
+Another possible approach for O(n): (only algo)
+
+1) Find inorder and preorder traversals of T, store them in two auxiliary arrays inT[] and preT[].
+
+2) Find inorder and preorder traversals of S, store them in two auxiliary arrays inS[] and preS[].
+
+3) If inS[] is a subarray of inT[] and preS[] is a subarray preT[], then S is a subtree of T. Else not.
+
+We can also use postorder traversal in place of preorder in the above algorithm.
+
+Let us consider the above example
+
+Inorder and Preorder traversals of the big tree are.
+inT[]   =  {a, c, x, b, z, e, k}
+preT[]  =  {z, x, a, c, b, e, k}
+
+Inorder and Preorder traversals of small tree are
+inS[]  = {a, c, x, b}
+preS[] = {x, a, c, b}
+
+We can easily figure out that inS[] is a subarray of
+inT[] and preS[] is a subarray of preT[]. 
