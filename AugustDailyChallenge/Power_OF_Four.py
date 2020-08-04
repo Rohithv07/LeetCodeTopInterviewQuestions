a845b82c54666ad7e2f35565ce922e437123532b
rohithv07 <rohithv63@gmail.com>
2020-08-04 13:27:01 +0530

    August Day 4

---
+++ AugustDailyChallenge/Power_OF_Four.py
@@ -0,0 +1,23 @@
+Power of Four
+
+
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 5
+Output: false
+
+
+Solution:
+
+class Solution:
+    def isPowerOfFour(self, num: int) -> bool:
+        if num < 1:
+            return False
+        logarithmic_check = math.log(num, 4)
+        return logarithmic_check == int(logarithmic_check)
