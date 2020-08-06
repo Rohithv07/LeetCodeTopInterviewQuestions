9ed3a27b62ca97dcaab263dca7e60567a1526d68
rohithv07 <rohithv63@gmail.com>
2020-08-06 12:49:43 +0530

    August Daily Challenge day 6

---
+++ AugustDailyChallenge/Find_All_Duplicates_In_An_Array.py
@@ -0,0 +1,24 @@
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+Could you do it without extra space and in O(n) runtime?
+
+Example:
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
+
+
+Solution:
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        li = []
+        for element, counter in Counter(nums).items():
+            if counter > 1:
+                li.append(element)
+                
+        return li
+        
