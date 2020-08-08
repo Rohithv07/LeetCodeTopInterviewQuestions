f2cb12781961e454648cca8436fe6168486d49d2
rohithv07 <rohithv63@gmail.com>
2020-08-08 17:52:49 +0530

    Medium Tree Section Java Iterative

---
+++ Tree/medium/BST_Inorder_Traversal.java
@@ -0,0 +1,51 @@
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+SOlution:
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList();
+        if (root == null)
+            return result;
+        Stack<TreeNode> stack = new Stack();
+        while(!stack.isEmpty() || root != null){
+            if (root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            else {
+                root = stack.pop();
+                result.add(root.val);
+                root = root.right;
+            }
+        }
+        return result;
+    }
+}
