d14a4a21ad7ca65f571f2aeb6668420f8b764141
rohithv07 <rohithv63@gmail.com>
2020-08-08 17:51:15 +0530

    Medium Section Python Recursive

---
+++ Tree/medium/BST_Inorder_Traversal.py
@@ -0,0 +1,34 @@
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+Solution:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        def recursive(node, li):
+            if node:
+                recursive(node.left, li)
+                li.append(node.val)
+                recursive(node.right, li)
+                
+            return li
+        
+        return recursive(root, res)
