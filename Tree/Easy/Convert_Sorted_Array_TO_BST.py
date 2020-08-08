7cfc88076ea3af38a8d84d97e93476768a50a49e
rohithv07 <rohithv63@gmail.com>
2020-08-08 17:50:32 +0530

    Tree Easy Section

---
+++ Tree/Easy/Convert_Sorted_Array_TO_BST.py
@@ -0,0 +1,35 @@
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+
+
+Solution:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        def recursive(low, high):
+            if low == high:
+                return None 
+            mid = (low + high) // 2
+            return TreeNode(nums[mid], recursive(low, mid ), recursive(mid + 1, high))
+        
+        return recursive(0, len(nums))
