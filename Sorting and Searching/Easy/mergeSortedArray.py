4459e7b0216e5e6753d9057f143456ed640d7348
rohithv07 <rohithv63@gmail.com>
2020-08-08 23:57:52 +0530

    Searching and sorting section easy

---
+++ Sorting and Searching/Easy/mergeSortedArray.py
@@ -0,0 +1,29 @@
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+
+Solution:
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        #result = nums1[:m]
+        #nums1.clear()
+        #nums1.extend(result)
+        #nums1.extend(nums2)
+        #nums1.sort()
+        
+        for i in range(m, m+n):
+            nums1[i] = nums2[i - m]
+        nums1.sort()
