91468b33d3e0a707bf79c27bf82b9802bbebee5b
rohithv07 <rohithv63@gmail.com>
2020-08-11 10:47:58 +0530

    Medium section Kth largest element Python

---
+++ Sorting and Searching/Medium/kthLargestElement.py
@@ -0,0 +1,21 @@
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+Note:
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+Solution:
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        if len(nums) >= 2:
+            return nums[-k]
+        return nums[0]
