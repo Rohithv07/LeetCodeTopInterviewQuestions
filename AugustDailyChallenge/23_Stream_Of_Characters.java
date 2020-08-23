0d649cdc37812a76ee1d8c918b0866cc077e4582
rohithv07 <rohithv63@gmail.com>
2020-08-23 13:23:51 +0530

    Day 23 Added question Java

--- AugustDailyChallenge/23_Stream_Of_Characters.java
+++ AugustDailyChallenge/23_Stream_Of_Characters.java
@@ -1,27 +1,73 @@
-if (curr.next[word.charAt(i) - 'a'] == null) {
-                curr.next[word.charAt(i) - 'a'] = new Trie();
-            }
-            curr = curr.next[word.charAt(i) - 'a'];
+Implement the StreamChecker class as follows:
+
+StreamChecker(words): Constructor, init the data structure with the given words.
+query(letter): returns true if and only if for some k >= 1, the last k characters queried (in order from oldest to newest, including this letter just queried) spell one of the words in the given list.
+ 
+
+Example:
+
+StreamChecker streamChecker = new StreamChecker(["cd","f","kl"]); // init the dictionary.
+streamChecker.query('a');          // return false
+streamChecker.query('b');          // return false
+streamChecker.query('c');          // return false
+streamChecker.query('d');          // return true, because 'cd' is in the wordlist
+streamChecker.query('e');          // return false
+streamChecker.query('f');          // return true, because 'f' is in the wordlist
+streamChecker.query('g');          // return false
+streamChecker.query('h');          // return false
+streamChecker.query('i');          // return false
+streamChecker.query('j');          // return false
+streamChecker.query('k');          // return false
+streamChecker.query('l');          // return true, because 'kl' is in the wordlist
+ 
+
+Note:
+
+1 <= words.length <= 2000
+1 <= words[i].length <= 2000
+Words will only consist of lowercase English letters.
+Queries will only consist of lowercase English letters.
+The number of queries is at most 40000.
+
+Solution:
+
+class StreamChecker {
+    class Trie {
+        boolean word;
+        Trie[] next;
+        public Trie() {
+            this.next = new Trie[26];
+            this.word = false;
         }
-        curr.word = true;
+    }
+    Trie root = null;
+    int p = 0;
+    char [] history;
+    public void add(String word) {
+        Trie current = root;
+        for (int i=word.length()-1; i>=0; i--){
+            if (current.next[word.charAt(i) - 'a'] == null) {
+                current.next[word.charAt(i) - 'a'] = new Trie();
+            }
+            current = current.next[word.charAt(i) - 'a'];
+        }
+        current.word = true;
     }
     
     public StreamChecker(String[] words) {
         this.root = new Trie();
         for (String word : words) {
-            insert(word);
+            add(word);
         }
         this.history = new char[2000];
     }
     
     public boolean query(char letter) {
-        // System.out.println("query " + letter);
         history[p % 2000] = letter;
         Trie curr = root;
         boolean match = false;
         for (int i = 0; i < 2000 && !match; i++) {
             char curChar = history[(p - i + 2000) % 2000];
-            // System.out.println("look " + curChar);
             if (curChar == 0) {
                 break;
             }
@@ -29,7 +75,6 @@ if (curr.next[word.charAt(i) - 'a'] == null) {
                 curr = curr.next[curChar - 'a'];
                 if (curr.word) {
                     match = true;
-                    // System.out.println("match " + curChar);
                 }
             } else {
                 break;
@@ -38,4 +83,11 @@ if (curr.next[word.charAt(i) - 'a'] == null) {
         p++;
         return match;
     }
+
 }
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker obj = new StreamChecker(words);
+ * boolean param_1 = obj.query(letter);
+ */
