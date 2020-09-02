97aa70f7b0bdc8a35ddcc8c9f52d29685ea55e57
rohithv07 <rohithv63@gmail.com>
2020-09-03 00:45:09 +0530

    Added question

--- Daily Byte/cycleStartFinding.py
+++ Daily Byte/cycleStartFinding.py
@@ -1,3 +1,11 @@
+This question is asked by Apple. Given a potentially cyclical linked list where each value is unique, return the node at which the cycle starts. If the list does not contain a cycle, return null.
+
+Ex: Given the following linked lists...
+
+1->2->3, return null
+1->2->3->4->5->2 (5 points back to 2), return a reference to the node containing 2
+1->9->3->7->7 (7 points to itself), return a reference to the node containing 7
+
 class ListNode:
 	def __init__(self, x):
 		self.x = x
