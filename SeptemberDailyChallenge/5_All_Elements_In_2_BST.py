1d0542e4a491a70e153f1ad93db712dfd95f4048
rohithv07 <rohithv63@gmail.com>
2020-09-05 14:19:36 +0530

    Day 5 Challenge Python

---
+++ SeptemberDailyChallenge/5_All_Elements_In_2_BST.py
@@ -0,0 +1,58 @@
+Given two binary search trees root1 and root2.
+
+Return a list containing all the integers from both trees sorted in ascending order.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [2,1,4], root2 = [1,0,3]
+Output: [0,1,1,2,3,4]
+Example 2:
+
+Input: root1 = [0,-10,10], root2 = [5,1,7,0,2]
+Output: [-10,0,0,1,2,5,7,10]
+Example 3:
+
+Input: root1 = [], root2 = [5,1,7,0,2]
+Output: [0,1,2,5,7]
+Example 4:
+
+Input: root1 = [0,-10,10], root2 = []
+Output: [-10,0,10]
+Example 5:
+
+
+Input: root1 = [1,null,8], root2 = [8,1]
+Output: [1,1,8,8]
+ 
+
+Constraints:
+
+Each tree has at most 5000 nodes.
+Each node's value is between [-10^5, 10^5].
+
+
+Solution:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        def postOrder(node, array):
+            if node is not None:
+                postOrder(node.left, array)
+                postOrder(node.right, array)
+                array.append(node.val)
+                
+        root1List = []
+        root2List = []
+        postOrder(root1, root1List)
+        postOrder(root2, root2List)
+        root1List.extend(root2List)
+        return sorted(root1List)
