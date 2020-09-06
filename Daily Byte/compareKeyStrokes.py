40491400460a141d627d1a663c58dfb6e1c40fd6
rohithv07 <rohithv63@gmail.com>
2020-09-06 17:25:29 +0530

    Daily byte question asked in amazon python

--- Daily Byte/compareKeyStrokes.py
+++ Daily Byte/compareKeyStrokes.py
@@ -1,3 +1,12 @@
+This question is asked by Amazon. Given two strings s and t, which represents a sequence of keystrokes, where # denotes a backspace, return whether or not the sequences produce the same result.
+
+Ex: Given the following strings...
+
+s = "ABC#", t = "CD##AB", return true
+s = "como#pur#ter", t = "computer", return true
+s = "cof#dim#ng", t = "code", return false
+
+
 from collections import deque
 def buildString(s):
 	stack = deque()
