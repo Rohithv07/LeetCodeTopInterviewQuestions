010459dd165b0894838c6061496490a0c661c4b0
rohithv07 <rohithv63@gmail.com>
2020-09-06 17:06:28 +0530

    Daily Byte question asked in google added question

--- Daily Byte/validParanthesisAnagram.py
+++ Daily Byte/validParanthesisAnagram.py
@@ -1,3 +1,13 @@
+This question is asked by Google. Given a string only containing the following characters (, ), {, }, [, and ] return whether or not the opening and closing characters are in a valid order.
+
+Ex: Given the following strings...
+
+"(){}[]", return true
+"(({[]}))", return true
+"{(})", return false
+
+
+
 from collections import deque
 
 def validBrackets(s):
