7825ea744c952a78b8de23e77eeb9a2899dbcc54
rohithv07 <rohithv63@gmail.com>
2020-09-14 17:10:21 +0530

    Hackerrank Recursion Problem Java

---
+++ HackerRank/recursionDavisStairCase.java
@@ -0,0 +1,57 @@
+https://www.hackerrank.com/challenges/ctci-recursive-staircase/problem
+
+Solution:
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the stepPerms function below.
+    static int stepPerms(int n) {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        if (n == 3)
+            return 4;
+        int [] dp = new int [n];
+        dp[0] = 1;
+        dp[1] = 2;
+        dp[2] = 4;
+        for (int i=3; i<n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n-1];
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int s = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int sItr = 0; sItr < s; sItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int res = stepPerms(n);
+
+            bufferedWriter.write(String.valueOf(res));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
