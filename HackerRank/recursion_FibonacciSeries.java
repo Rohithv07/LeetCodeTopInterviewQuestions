7552ca0ddcad1c094d0e11607cb81a1aa51ebdaf
rohithv07 <rohithv63@gmail.com>
2020-09-14 17:00:25 +0530

    Hackerrank recursion section problem Java

---
+++ HackerRank/recursion_FibonacciSeries.java
@@ -0,0 +1,27 @@
+https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=recursion-backtracking
+
+
+Solution:
+
+import java.util.*;
+
+public class Solution {
+
+    public static int fibonacci(int n) {
+        int [] dp = new int [n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
+}
