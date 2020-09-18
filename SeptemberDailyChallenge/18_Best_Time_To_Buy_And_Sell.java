ea60ba1a59a9dca0a1358e41bee0cf7edeed09a6
rohithv07 <rohithv63@gmail.com>
2020-09-18 13:27:17 +0530

    Day 18 Problem Java

---
+++ SeptemberDailyChallenge/18_Best_Time_To_Buy_And_Sell.java
@@ -0,0 +1,33 @@
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+Solution:
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0)
+            return 0;
+        int minimum = prices[0];
+        int maximum = 0;
+        for (int price: prices) {
+            minimum = Math.min(minimum, price);
+            maximum = Math.max(maximum, price-minimum);
+        }
+        return maximum;
+    }
+}s
