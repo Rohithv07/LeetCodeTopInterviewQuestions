5aeb8f8754a31a17f6c11fb3fa9a48e8d2855ce5
rohithv07 <rohithv63@gmail.com>
2020-09-20 17:14:02 +0530

    Daily byte probklem java

--- Daily Byte/sortedArrayToBst.java
+++ Daily Byte/sortedArrayToBst.java
@@ -1,3 +1,17 @@
+Given an array of numbers sorted in ascending order, return a height balanced binary search tree using every number from the array. Note: height balanced meaning that the level of any node’s two subtrees should not differ by more than one.
+
+Ex: nums = [1, 2, 3] return a reference to the following tree...
+       2
+      /  \
+     1    3
+Ex: nums = [1, 2, 3, 4, 5, 6] return a reference to the following tree...
+        3
+       / \
+     2    5
+    /   /  \
+  1    4    6
+
+
 
 public class TreeNode {
 	int val;
