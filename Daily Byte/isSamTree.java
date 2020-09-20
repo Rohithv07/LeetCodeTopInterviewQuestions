3ee174c3982f15f621cc429fe7fe8e752d86cc9a
rohithv07 <rohithv63@gmail.com>
2020-09-20 17:26:19 +0530

    Daily byte problem Java

---
+++ Daily Byte/isSamTree.java
@@ -0,0 +1,62 @@
+Given two binary trees, return whether or not the two trees are identical. Note: identical meaning they exhibit the same structure and the same values at each node. Ex: Given the following trees...
+
+        2
+       / \
+      1   3
+    2
+   / \
+  1   3
+
+return true.
+
+Ex: Given the following trees...
+
+        1
+         \
+          9
+           \
+           18
+    1
+   /
+  9
+   \
+    18
+
+return false.
+
+Ex: Given the following trees...
+
+        2
+       / \
+      3   1
+    2
+   / \
+  1   3
+
+return false. 
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(){}
+	TreeNode(int val) {
+		this.val = val;
+	}
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+class Solution {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null)
+			return true;
+		if (p == null && q != null || p != null && q == null)
+			return false;
+		if (p.val != q.val)
+			return false;
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
+}
