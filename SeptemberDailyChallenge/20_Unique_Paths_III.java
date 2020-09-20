d9822080a0e0c9d0289aad0b6292ac72c61d34ac
rohithv07 <rohithv63@gmail.com>
2020-09-20 14:13:05 +0530

    day 20 Solution java

--- SeptemberDailyChallenge/20_Unique_Paths_III.java
+++ SeptemberDailyChallenge/20_Unique_Paths_III.java
@@ -1,3 +1,41 @@
+On a 2-dimensional grid, there are 4 types of squares:
+
+1 represents the starting square.  There is exactly one starting square.
+2 represents the ending square.  There is exactly one ending square.
+0 represents empty squares we can walk over.
+-1 represents obstacles that we cannot walk over.
+Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
+
+ 
+
+Example 1:
+
+Input: [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+Output: 2
+Explanation: We have the following two paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+Example 2:
+
+Input: [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+Output: 4
+Explanation: We have the following four paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+Example 3:
+
+Input: [[0,1],[2,0]]
+Output: 0
+Explanation: 
+There is no path that walks over every empty square exactly once.
+Note that the starting and ending square can be anywhere in the grid.
+
+
+
+
+
 /* My thought process
 For most of the grid problem, the common approach is to use the dfs approach. So I initially coded the dfs function, written the exiting condition and all the dfsMovement condition to left, right, top, down and finally when I run it, I was not gettin the correct output. 
 So I just looks through the disccus section and there is solutions with the backtracking applied. So I once again looked and find that we need to find all the possible path and get the total count. Yes, eventhogh we found a path, we need to again start from the beginning to search for another possible path. SO we need to use thebacktracking along wiht the dfs.
