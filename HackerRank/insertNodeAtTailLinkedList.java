2a26a74291a589760c0fda078650e65098049654
rohithv07 <rohithv63@gmail.com>
2020-10-15 11:40:05 +0530

    hackerrank solution in java

---
+++ HackerRank/insertNodeAtTailLinkedList.java
@@ -0,0 +1,74 @@
+https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+      
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+        if (head == null) {
+            head = new SinglyLinkedListNode(data);
+            return head;
+        }
+        newNode.next = null;
+        SinglyLinkedListNode last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = newNode;
+        //head.next = null;
+        return head;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
