1f06dde858efc89ee056e9de1c7a68e4ae735ffc
rohithv07 <rohithv63@gmail.com>
2020-10-15 16:58:21 +0530

    Hackerrank solution in java

---
+++ HackerRank/deleteNodeFromLinkedList.java
@@ -0,0 +1,87 @@
+https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the deleteNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode deleteNode(SinglyLinkedListNode head, int position) {
+        if (head == null)
+            return head;
+        if (position == 0) {
+            head = head.next;
+        }else {
+        SinglyLinkedListNode temp = head;
+        for (int i=0; i<position-1; i++) {
+            temp = temp.next;
+        }
+        if (temp.next == null)
+            return head;
+        temp.next = temp.next.next;
+        }
+        return head;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
