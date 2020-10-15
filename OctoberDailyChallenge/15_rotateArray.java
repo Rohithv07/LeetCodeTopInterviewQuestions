8ab522e932b3bba16c1238d555e32d7bf92155c6
rohithv07 <rohithv63@gmail.com>
2020-10-15 19:55:28 +0530

    Leetcode day 15 Problem Java

---
+++ OctoberDailyChallenge/15_rotateArray.java
@@ -0,0 +1,42 @@
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Follow up:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+    public void reverse(int [] nums, int low, int high) {
+        while (low < high) {
+            int temp = nums[low];
+            nums[low] = nums[high];
+            nums[high] = temp;
+            low += 1;
+            high -= 1;
+        }
+    }
+}
