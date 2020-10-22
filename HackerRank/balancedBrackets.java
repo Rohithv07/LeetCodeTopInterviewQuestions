964e81a513afb1897830942b056ad183ead92f93
rohithv07 <rohithv63@gmail.com>
2020-10-22 17:41:36 +0530

    Hackerrank balance bracket solution java

---
+++ HackerRank/balancedBrackets.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the isBalanced function below.
+    static String isBalanced(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+        Stack<Character> stack = new Stack<>();
+        for (char ch: s.toCharArray()) {
+            if (map.containsKey(ch)) {
+                stack.push(ch);
+            }
+            else if (!stack.isEmpty()) {
+                if (map.get(stack.pop()) != ch)
+                    return "NO";
+            }
+            else
+                return "NO";
+        }
+        return stack.isEmpty() ? "YES" : "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String s = scanner.nextLine();
+
+            String result = isBalanced(s);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
