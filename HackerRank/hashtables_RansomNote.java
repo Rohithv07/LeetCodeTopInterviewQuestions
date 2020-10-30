8fb88dcc5d402a97fc8d48207a00b8223dc4002e
rohithv07 <rohithv63@gmail.com>
2020-10-30 17:00:11 +0530

    Hackerrank solution for hashtable problem Java

---
+++ HackerRank/hashtables_RansomNote.java
@@ -0,0 +1,69 @@
+https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmapsimport java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+        Map<String, Integer> map = new HashMap<>();
+        boolean flag = true;
+        for (String s: magazine) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        for (String s: note) {
+            if (!map.containsKey(s) || map.get(s) == 0) {
+                flag = false;
+                break;
+            }
+            else {
+                map.replace(s, map.get(s) - 1);
+            }
+
+        }
+        if (flag)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+        
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] note = new String[n];
+
+        String[] noteItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String noteItem = noteItems[i];
+            note[i] = noteItem;
+        }
+
+        checkMagazine(magazine, note);
+
+        scanner.close();
+    }
+}
+
