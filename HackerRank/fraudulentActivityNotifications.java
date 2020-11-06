c51885475b830614901651637d4aea52c5d1a648
rohithv07 <rohithv63@gmail.com>
2020-11-06 12:36:16 +0530

    Hackerrqank problem in java

---
+++ HackerRank/fraudulentActivityNotifications.java
@@ -0,0 +1,93 @@
+https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the activityNotifications function below.
+    static int activityNotifications(int[] expenditure, int d) {
+        int noticeCount = 0;
+        int [] data = new int [201];
+        // counting sort
+        for (int i=0; i<d; i++)
+            data[expenditure[i]] += 1;
+        for (int i=d; i<expenditure.length; i++) {
+            double median = getMedian(d, data);
+            if (expenditure[i] >= 2*median)
+                noticeCount += 1;
+            data[expenditure[i]] += 1;
+            data[expenditure[i-d]] -= 1;
+        }
+        return noticeCount;
+    }
+    static double getMedian(int d, int [] data) {
+        double median = 0;
+        if (d % 2 == 0) {
+            Integer m1 = null;
+            Integer m2 = null;
+            int count = 0;
+            for (int j=0; j<data.length; j++) {
+                count += data[j];
+                if (m1 == null && count >= d/2) {
+                    m1 = j;
+                }
+                if (m2 == null && count >= d/2+1) {
+                    m2 = j;
+                    break;
+                }   
+            }
+            median = (m1 + m2) / 2.0;
+        }
+        else {
+            int count = 0;
+            for (int j=0; j<data.length; j++) {
+                count += data[j];
+                if (count > d/2) {
+                    median = j;
+                    break;
+                }
+            }
+        }
+        return median;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] expenditure = new int[n];
+
+        String[] expenditureItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int expenditureItem = Integer.parseInt(expenditureItems[i]);
+            expenditure[i] = expenditureItem;
+        }
+
+        int result = activityNotifications(expenditure, d);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
