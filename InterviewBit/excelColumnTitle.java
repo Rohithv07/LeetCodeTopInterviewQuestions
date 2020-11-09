d2016c2e79ab040c0d67334c305a6c6d0aa49c4f
rohithv07 <rohithv63@gmail.com>
2020-11-09 17:30:20 +0530

    Interviewbit problem 2 solution in amazon in java

--- InterviewBit/excelColumnTitle.java
+++ InterviewBit/excelColumnTitle.java
@@ -4,3 +4,16 @@ public class Solution {
     }
 }
 
+
+
+public class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder result = new StringBuilder();
+        while (n-->0) {
+            result.insert(0, (char)('A' + n % 26));
+            n /= 26;
+        }
+        return result.toString();
+    }
+}
+
