8476d46161c07f5c0105952f638b1bd769d5f30b
rohithv07 <rohithv63@gmail.com>
2020-11-09 17:44:25 +0530

    Interviewbit problem asked in hcl, amazon java

---
+++ InterviewBit/reverseInteger.py
@@ -0,0 +1,17 @@
+class Solution:
+	# @param A : integer
+	# @return an integer
+	def reverse(self, A):
+        B = abs(A)
+        a = list(str(B))
+        a.reverse()
+        c = ""
+        c = int(c.join(a))
+        if c > 2 ** 31:
+            return 0
+        else:
+            if A >= 0:
+                return c
+            else:
+                return -c
+
