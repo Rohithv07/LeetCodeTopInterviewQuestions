1a305cced247a8cf157665945e5124273921d88a
rohithv07 <rohithv63@gmail.com>
2020-11-14 17:38:35 +0530

    Daily byte problem asked in facebook java

---
+++ Daily Byte/compression.java
@@ -0,0 +1,18 @@
+public class Solution {
+public int compress(char[] chars) {
+        int indexAns = 0, index = 0;
+        while(index < chars.length){
+            char currentChar = chars[index];
+            int count = 0;
+            while(index < chars.length && chars[index] == currentChar){
+                index++;
+                count++;
+            }
+            chars[indexAns++] = currentChar;
+            if(count != 1)
+                for(char c : Integer.toString(count).toCharArray()) 
+                    chars[indexAns++] = c;
+        }
+        return indexAns;
+    }
+}
