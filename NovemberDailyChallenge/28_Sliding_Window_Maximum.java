02d16f7b7ca5586998c8ed6528a33320a3b5319a
rohithv07 <rohithv63@gmail.com>
2020-11-28 15:09:42 +0530

    Leetcode day 28 problem java

---
+++ NovemberDailyChallenge/28_Sliding_Window_Maximum.java
@@ -0,0 +1,65 @@
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Example 3:
+
+Input: nums = [1,-1], k = 1
+Output: [1,-1]
+Example 4:
+
+Input: nums = [9,11], k = 2
+Output: [11]
+Example 5:
+
+Input: nums = [4,-2], k = 2
+Output: [4]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+
+
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int length = nums.length;
+        int [] result = new int [length - k + 1];
+        int resultIndex = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i=0; i<length; i++) {
+            // check for the out of bounds
+            while (queue.size() > 0 && queue.peekFirst() <= i-k)
+                queue.pollFirst();
+            // remove elemnts that are currentlu less than nums[i]
+            while (queue.size() > 0 && nums[queue.peekLast()] < nums[i])
+                queue.pollLast();
+            queue.offerLast(i);
+            if (i >= k - 1)
+                result[i - k + 1] = nums[queue.peekFirst()];
+        }
+        return result;
+    }
+}
