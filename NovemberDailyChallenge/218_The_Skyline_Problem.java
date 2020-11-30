b795ffc9425fc2a26bfd517f953f27942a3c5e18
rohithv07 <rohithv63@gmail.com>
2020-11-30 15:31:58 +0530

    Leetcode 218 problem in java

---
+++ NovemberDailyChallenge/218_The_Skyline_Problem.java
@@ -0,0 +1,33 @@
+public List<int[]> getSkyline(int[][] buildings) {
+    List<int[]> result = new ArrayList<>();
+    List<int[]> height = new ArrayList<>();
+    for(int[] b:buildings) {
+        height.add(new int[]{b[0], -b[2]});
+        height.add(new int[]{b[1], b[2]});
+    }
+    Collections.sort(height, (a, b) -> {
+            if(a[0] != b[0]) 
+                return a[0] - b[0];
+            return a[1] - b[1];
+    });
+    Queue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+    pq.offer(0);
+    int prev = 0;
+    for(int[] h:height) {
+        if(h[1] < 0) {
+            pq.offer(-h[1]);
+        } else {
+            pq.remove(h[1]);
+        }
+        int cur = pq.peek();
+        if(prev != cur) {
+            result.add(new int[]{h[0], cur});
+            prev = cur;
+        }
+    }
+    return result;
+}
+
+
+
+// please see tushar roys video for revising again
