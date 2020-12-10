af5938d30c8f9a408c0cc2f1493a4229871fc64f
rohithv07 <rohithv63@gmail.com>
2020-12-10 17:20:44 +0530

    Leetcode day 10 problem in java

---
+++ DecmeberDailyChallenge/10_Valid_Mountain_Array.java
@@ -0,0 +1,44 @@
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < A[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+ 
+
+Example 1:
+
+Input: arr = [2,1]
+Output: false
+Example 2:
+
+Input: arr = [3,5,5]
+Output: false
+Example 3:
+
+Input: arr = [0,3,2,1]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 104
+0 <= arr[i] <= 104
+
+
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int length = arr.length;
+        int index = 0;
+        while (index + 1 < length && arr[index] < arr[index+1])
+            index += 1;
+        if (index == 0 || index == length - 1) // peak can't be at the first or last
+            return false;
+        while (index + 1 < length && arr[index] > arr[index+1])
+            index += 1;
+        return index == length - 1;
+    }
+}
