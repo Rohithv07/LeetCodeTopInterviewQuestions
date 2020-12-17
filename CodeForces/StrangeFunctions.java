be51b047c3616277cff9947900a619f1171402ca
rohithv07 <rohithv63@gmail.com>
2020-12-17 11:42:55 +0530

    Codeforces 1455A Strange functions java

---
+++ CodeForces/StrangeFunctions.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class StrangeFunctions {
+
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		while (test-- > 0) {
+			String s = sc.next();
+			System.out.println(s.length());
+
+		}
+	}
+}
