2dc35cf88b685b3142d7c917a931dd3dea430e32
rohithv07 <rohithv63@gmail.com>
2020-12-25 22:46:55 +0530

    Hackerrank solution in java

---
+++ HackerRank/HashTables_IceCreamParlor.java
@@ -0,0 +1,58 @@
+https://www.hackerrank.com/challenges/ctci-ice-cream-parlor/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=search
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the whatFlavors function below.
+    static void whatFlavors(int[] cost, int money) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<cost.length; i++) {
+            if (!map.containsKey(cost[i]))
+                map.put(money - cost[i], i+1);
+            else {
+                System.out.print(map.get(cost[i]) + " " + (i + 1));
+                System.out.println();
+                return;
+            }
+        }
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int money = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] cost = new int[n];
+
+            String[] costItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int costItem = Integer.parseInt(costItems[i]);
+                cost[i] = costItem;
+            }
+
+            whatFlavors(cost, money);
+        }
+
+        scanner.close();
+    }
+}
+
