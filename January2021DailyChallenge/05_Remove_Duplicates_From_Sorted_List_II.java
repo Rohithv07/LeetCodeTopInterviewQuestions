7fd17d8baaacc5b9242f0f82e6cb4f35e0429025
rohithv07 <rohithv63@gmail.com>
2021-01-05 19:22:57 +0530

    Leetcode day 5 problem java

---
+++ January2021DailyChallenge/05_Remove_Duplicates_From_Sorted_List_II.java
@@ -0,0 +1,48 @@
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
+Example 2:
+
+
+Input: head = [1,1,1,2,3]
+Output: [2,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        int currentValue = head.val;
+        if (currentValue != head.next.val) {
+            head.next = deleteDuplicates(head.next);
+            return head;
+        }
+        while (head != null && (currentValue == head.val))
+            head = head.next;
+        return deleteDuplicates(head);
+        
+    }
+}
