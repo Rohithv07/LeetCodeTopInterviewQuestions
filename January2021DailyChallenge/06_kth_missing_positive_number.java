da18c34558c75552e6a147b7386e9403c1dd2722
rohithv07 <rohithv63@gmail.com>
2021-01-06 19:32:16 +0530

    Leetcode day 6 problem java

---
+++ January2021DailyChallenge/06_kth_missing_positive_number.java
@@ -0,0 +1,58 @@
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+Find the kth positive integer that is missing from this array.
+
+ 
+
+Example 1:
+
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+Example 2:
+
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+1 <= k <= 1000
+arr[i] < arr[j] for 1 <= i < j <= arr.length
+
+
+
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        // Naive
+        int lastElementOfArray = arr[0];
+        int lastPossible = arr[arr.length-1] + k;
+        int possibleNumber = 1;
+        Set<Integer> set = new HashSet<>();
+        for (int number : arr)
+            set.add(number);
+        List<Integer> missingNumbers = new ArrayList<>();
+        for (int i=possibleNumber; i<=lastPossible; i++) {
+            if (!set.contains(i))
+                missingNumbers.add(i);
+        }
+        return missingNumbers.get(k-1);
+        
+        
+        // binary search
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+            if (arr[middle] - (middle + 1) >= k)
+                right = middle;
+            else
+                left = middle + 1;
+        }
+        return left + k;
+    }
+}
