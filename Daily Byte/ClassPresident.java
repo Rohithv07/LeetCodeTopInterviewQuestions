ba71a2a9fd0f0386d0d9797eca8c56aa134602ce
rohithv07 <rohithv63@gmail.com>
2021-01-14 10:59:02 +0530

    Daily byte problem asked in interviews

--- Daily Byte/ClassPresident.java
+++ Daily Byte/ClassPresident.java
@@ -1,3 +1,14 @@
+High school students are voting for their class president and you’re tasked with counting the votes. Each presidential candidates is represented by a unique integer and the candidate that should win the election is the candidate that has received more than half the votes. Given a list of integers, return the candidate that should become the class president.
+Note: You may assume there is always a candidate that has received more than half the votes.
+
+Ex: Given the following votes…
+
+votes = [1, 1, 2, 2, 1], return 1.
+Ex: Given the following votes…
+
+votes = [1, 3, 2, 3, 1, 2, 3, 3, 3], return 3.
+
+
 import java.util.*;
 
 public class ClassPresident {
