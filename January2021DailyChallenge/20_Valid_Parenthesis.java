e93175d855e552972075af13f492c34397b93f27
rohithv07 <rohithv63@gmail.com>
2021-01-20 17:00:58 +0530

    Leetcode day 20 problem java

---
+++ January2021DailyChallenge/20_Valid_Parenthesis.java
@@ -0,0 +1,60 @@
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+Example 4:
+
+Input: s = "([)]"
+Output: false
+Example 5:
+
+Input: s = "{[]}"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+
+
+
+
+
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() < 2)
+            return false;
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(')
+                stack.push(')');
+            else if (ch == '{')
+                stack.push('}');
+            else if (ch == '[')
+                stack.push(']');
+            else {
+                if (stack.isEmpty() || ch != stack.peek())
+                    return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty() ? true : false;
+    }
+}
