932825f5cf1cf3c1ed20a8892735479aa9a6caca
rohithv07 <rohithv63@gmail.com>
2021-02-27 16:07:24 +0530

    Leetcode day 27 problem java

---
+++ FebruaryDailyChallenge2021/27_Dividwe_Two_Integers.java
@@ -0,0 +1,51 @@
+Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero, which means losing its fractional part. For example, truncate(8.345) = 8 and truncate(-2.7335) = -2.
+
+Note:
+
+Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For this problem, assume that your function returns 231 − 1 when the division result overflows.
+ 
+
+Example 1:
+
+Input: dividend = 10, divisor = 3
+Output: 3
+Explanation: 10/3 = truncate(3.33333..) = 3.
+Example 2:
+
+Input: dividend = 7, divisor = -3
+Output: -2
+Explanation: 7/-3 = truncate(-2.33333..) = -2.
+Example 3:
+
+Input: dividend = 0, divisor = 1
+Output: 0
+Example 4:
+
+Input: dividend = 1, divisor = 1
+Output: 1
+ 
+
+Constraints:
+
+-231 <= dividend, divisor <= 231 - 1
+divisor != 0
+
+
+
+
+class Solution {
+    public int divide(int A, int B) {
+        if (A == 1 << 31 && B == -1) return (1 << 31) - 1;
+        int a = Math.abs(A), b = Math.abs(B), res = 0;
+        for (int x = 31; x >= 0; x--)
+            if ((a >>> x) - b >= 0) {
+                res += 1 << x;
+                a -= b << x;
+            }
+        return (A > 0) == (B > 0) ? res : -res;
+    }
+}
