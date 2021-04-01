055e5465292c85893dd17e713808618d0e93b215
rohithv07 <rohithv63@gmail.com>
2021-04-01 14:13:33 +0530

    Leetcode day 1 problem java

---
+++ AprilDailyChallenge2021/01_Palindrome_Linked_List.java
@@ -0,0 +1,73 @@
+Given the head of a singly linked list, return true if it is a palindrome.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ 
+
+Follow up: Could you do it in O(n) time and O(1) space?
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+        // find the middle
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = head;
+        slow = reverseHelper(slow);
+        while (slow != null) {
+            if (slow.val != fast.val)
+                return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+        
+    }
+    
+    public ListNode reverseHelper(ListNode node) {
+        if (node == null)
+            return null;
+        ListNode current = node;
+        ListNode previous = null;
+        while (current != null) {
+            ListNode nextOfCurrent = current.next;
+            current.next = previous;
+            previous = current;
+            current = nextOfCurrent;
+        }
+        return previous;
+    }
+}
