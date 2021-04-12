b5e766f146c9869860760ee39f596df92a20ae4c
rohithv07 <rohithv63@gmail.com>
2021-04-12 16:54:49 +0530

    Daily byte problem asked in interviews in java

---
+++ Daily Byte/RemoveDuplicateValuesLinkedList.java
@@ -0,0 +1,34 @@
+/*
+Given a sorted singly linked list, remove all duplicate values and return the modified list.
+
+Ex: Given the following sorted singly linked list…
+
+1->2->2->3, return a list that looks as follows: 1->2->3.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            }
+            else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
