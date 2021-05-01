c17af667f25eecc28c46d0add6015a2245ae3843
rohithv07 <rohithv63@gmail.com>
2021-05-01 10:55:32 +0530

    Codeforces 1505A problem java

---
+++ CodeForces/IsItRated2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class IsItRated2 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			sc.nextLine();
+			System.out.println("no");
+		}
+	}
+}
