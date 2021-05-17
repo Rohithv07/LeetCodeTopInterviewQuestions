390a67f3834bb5a9b67ac14cead268d8fea91ab2
rohithv07 <rohithv63@gmail.com>
2021-05-17 14:57:26 +0530

    Daily byte problem asked in interviews in java

--- Daily Byte/MaxNumber.java
+++ Daily Byte/MaxNumber.java
@@ -21,3 +21,18 @@ class Solution {
         return Integer.valueOf(new String(digits));
     }
 }
+
+
+// Python solution without converting to String
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        i = 0 
+        tem = num
+        sixidx = -1 
+        while tem > 0:
+            if tem % 10 == 6:
+                sixidx = i  #refresh sixidx when found 6 at large digit.
+            tem = tem//10
+            i += 1
+        return (num + 3 *(10**sixidx)) if sixidx != -1 else num
