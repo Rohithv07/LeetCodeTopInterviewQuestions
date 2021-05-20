5a2b53d9c5ceaf0d3946e9e61e0ab69a60ecff45
rohithv07 <rohithv63@gmail.com>
2021-05-20 23:34:34 +0530

    Valid path updayed but not accepted

--- CodeChef/MayLongChallenge/ValidPaths.java
+++ CodeChef/MayLongChallenge/ValidPaths.java
@@ -3,16 +3,15 @@ public class ValidPaths {
 
 	private static final int N = (int)1e5 + 5;
 	private static int [] vertices = new int [N];
-	private static Map<Integer, List<Integer>> graph = new HashMap<>();
-	private static int result;
+	private static List<Integer>[] graph = new ArrayList[N];
+	private static int result = 0;
 	private static final int MOD = (int)1e9 + 7;
 
 
 	public static int dfs(int i, int j) {
 		vertices[i] = 1;
 		result += 1;
-		List<Integer> children = graph.get(i);
-		for (Integer child : children) {
+		for (Integer child : graph[i]) {
 			if (child == j)
 				continue;
 			dfs(child, i);
@@ -31,14 +30,13 @@ public class ValidPaths {
 			int n = sc.nextInt();
 			for (int i=1; i<=n; i++) {
 				vertices[i] = 0;
-				graph.remove(i);
+				graph[i] = new ArrayList<>();
 			}
 			for (int i=1; i<n; i++) {
 				int u = sc.nextInt();
 				int v = sc.nextInt();
-				graph.computeIfAbsent(u, x -> new ArrayList<>()).add(v);
-				graph.computeIfAbsent(v, x -> new ArrayList<>()).add(u);
-
+				graph[u].add(v);
+				graph[v].add(u);
 			}
 			result = 0;
 			dfs(1, 1);
