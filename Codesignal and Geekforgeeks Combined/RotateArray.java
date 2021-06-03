842b1b77e0b8e6d8ec3d4344a0d3d0876a0df1f6
rohithv07 <rohithv63@gmail.com>
2021-06-03 15:47:49 +0530

    GfG problem asked in amazon,microsfot in java

---
+++ Codesignal and Geekforgeeks Combined/RotateArray.java
@@ -0,0 +1,65 @@
+Given an unsorted array arr[] of size N, rotate it by D elements in the counter-clockwise direction. 
+
+ 
+
+Example 1:
+
+Input:
+N = 5, D = 2
+arr[] = {1,2,3,4,5}
+Output: 3 4 5 1 2
+Explanation: 1 2 3 4 5  when rotated
+by 2 elements, it becomes 3 4 5 1 2.
+Example 2:
+
+Input:
+N = 10, D = 3
+arr[] = {2,4,6,8,10,12,14,16,18,20}
+Output: 8 10 12 14 16 18 20 2 4 6
+Explanation: 2 4 6 8 10 12 14 16 18 20 
+when rotated by 3 elements, it becomes 
+8 10 12 14 16 18 20 2 4 6.
+ 
+
+Your Task:
+Complete the function rotateArr() which takes the array, D and N as input parameters and rotates the array by D elements. The array must be modified in-place without using extra space. 
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 <= N <= 107
+1 <= D <= N
+0 <= arr[i] <= 105
+
+
+class Solution
+{
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    static void rotateArr(int arr[], int d, int n)
+    {
+        // add your code here
+        if (n == 1)
+            return;
+        d = d % n;
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+    
+    static void reverse(int [] arr, int d, int n) {
+        int start = d;
+        int end = n;
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start ++;
+            end --;
+        }
+    }
+}
