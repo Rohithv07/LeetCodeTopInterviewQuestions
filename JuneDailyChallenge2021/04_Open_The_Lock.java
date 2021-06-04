7c6a8376a79833e6af26c2d47ce1edaba3d9a12e
rohithv07 <rohithv63@gmail.com>
2021-06-04 15:33:27 +0530

    Leetcode day 4 problem in java

---
+++ JuneDailyChallenge2021/04_Open_The_Lock.java
@@ -0,0 +1,123 @@
+You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
+
+The lock initially starts at '0000', a string representing the state of the 4 wheels.
+
+You are given a list of deadends dead ends, meaning if the lock displays any of these codes, the wheels of the lock will stop turning and you will be unable to open it.
+
+Given a target representing the value of the wheels that will unlock the lock, return the minimum total number of turns required to open the lock, or -1 if it is impossible.
+
+ 
+
+Example 1:
+
+Input: deadends = ["0201","0101","0102","1212","2002"], target = "0202"
+Output: 6
+Explanation:
+A sequence of valid moves would be "0000" -> "1000" -> "1100" -> "1200" -> "1201" -> "1202" -> "0202".
+Note that a sequence like "0000" -> "0001" -> "0002" -> "0102" -> "0202" would be invalid,
+because the wheels of the lock become stuck after the display becomes the dead end "0102".
+Example 2:
+
+Input: deadends = ["8888"], target = "0009"
+Output: 1
+Explanation:
+We can turn the last wheel in reverse to move from "0000" -> "0009".
+Example 3:
+
+Input: deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888"
+Output: -1
+Explanation:
+We can't reach the target without getting stuck.
+Example 4:
+
+Input: deadends = ["0000"], target = "8888"
+Output: -1
+ 
+
+Constraints:
+
+1 <= deadends.length <= 500
+deadends[i].length == 4
+target.length == 4
+target will not be in the list deadends.
+target and deadends[i] consist of digits only.
+
+
+class Solution {
+    public int openLock(String[] deadends, String target) {
+
+	// bfs - normal
+
+        // Queue<String> q = new LinkedList<>();
+        // Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+        // Set<String> visited = new HashSet<>();
+        // q.offer("0000");
+        // visited.add("0000");
+        // int level = 0;
+        // while(!q.isEmpty()) {
+        //     int size = q.size();
+        //     while(size > 0) {
+        //         String s = q.poll();
+        //         if(deads.contains(s)) {
+        //             size --;
+        //             continue;
+        //         }
+        //         if(s.equals(target)) return level;
+        //         StringBuilder sb = new StringBuilder(s);
+        //         for(int i = 0; i < 4; i ++) {
+        //             char c = sb.charAt(i);
+        //             String s1 = sb.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + sb.substring(i + 1);
+        //             String s2 = sb.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + sb.substring(i + 1);
+        //             if(!visited.contains(s1) && !deads.contains(s1)) {
+        //                 q.offer(s1);
+        //                 visited.add(s1);
+        //             }
+        //             if(!visited.contains(s2) && !deads.contains(s2)) {
+        //                 q.offer(s2);
+        //                 visited.add(s2);
+        //             }
+        //         }
+        //         size --;
+        //     }
+        //     level ++;
+        // }
+        // return -1;
+        
+        
+        
+        // 2 directional bfs
+         Set<String> begin = new HashSet<>();
+        Set<String> end = new HashSet<>();
+        Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+        begin.add("0000");
+        end.add(target);
+        int level = 0;
+        Set<String> temp;
+        while(!begin.isEmpty() && !end.isEmpty()) {
+            if (begin.size() > end.size()) {
+                temp = begin;
+                begin = end;
+                end = temp;
+            }
+            temp = new HashSet<>();
+            for(String s : begin) {
+                if(end.contains(s)) return level;
+                if(deads.contains(s)) continue;
+                deads.add(s);
+                StringBuilder sb = new StringBuilder(s);
+                for(int i = 0; i < 4; i ++) {
+                    char c = sb.charAt(i);
+                    String s1 = sb.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + sb.substring(i + 1);
+                    String s2 = sb.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + sb.substring(i + 1);
+                    if(!deads.contains(s1))
+                        temp.add(s1);
+                    if(!deads.contains(s2))
+                        temp.add(s2);
+                }
+            }
+            level ++;
+            begin = temp;
+        }
+        return -1;
+    }
+}
