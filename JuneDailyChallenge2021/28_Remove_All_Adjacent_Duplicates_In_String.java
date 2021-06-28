11a5a58f667c4cf1529eefd548deed877c7809e3
rohithv07 <rohithv63@gmail.com>
2021-06-28 14:50:19 +0530

    Leetcode day 28 problem java

---
+++ JuneDailyChallenge2021/28_Remove_All_Adjacent_Duplicates_In_String.java
@@ -0,0 +1,55 @@
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+Example 2:
+
+Input: s = "azxxzy"
+Output: "ay"
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+
+
+class Solution {
+    public String removeDuplicates(String s) {
+        if (s == null || s.length() == 0)
+            return "";
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(ch);
+            }
+            else if (stack.peek() == ch) {
+                stack.pop();
+            }
+            else {
+                stack.push(ch);
+            }
+        }
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
+
+/*
+"abbaca"
+
+[c a]
+*/
