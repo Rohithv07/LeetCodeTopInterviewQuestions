9c0720751c82d925fb0dfc3faf327300f4b9e4e3
Rohithv07 <rohithv63@gmail.com>
2021-07-22 12:41:30 +0530

    GfG problem asked in microsoft in java

---
+++ Codesignal and Geekforgeeks Combined/IterativeApproachInorder.java
@@ -0,0 +1,78 @@
+Given a binary tree. Find the inorder traversal of the tree without using recursion.
+
+Example 1
+
+Input:
+           1
+         /    \
+       2       3
+      /   \
+    4     5
+Output: 4 2 5 1 3
+Explanation:
+Inorder traversal (Left->Root->Right) of 
+the tree is 4 2 5 1 3.
+Example 2
+
+Input:
+            8
+          /   \
+            1      5
+             \    /  \
+             7   10   6
+             \  /
+          10 6
+Output: 1 7 10 8 6 10 5 6
+Explanation:
+Inorder traversal (Left->Root->Right) 
+of the tree is 1 7 10 8 6 10 5 6.
+ 
+
+Your task:
+You don't need to read input or print anything. Your task is to complete the function inOrder() which takes the root of the tree as input and returns a list containing the inorder traversal of the tree, calculated without using recursion.
+
+Expected time complexity: O(N)
+Expected auxiliary space: O(N)
+
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <= Data of a node <= 105
+
+
+//User function Template for Java
+
+/* A Binary Tree node 
+
+class Node {
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
+class Solution
+{
+    // Return a list containing the inorder traversal of the given tree
+    ArrayList<Integer> inOrder(Node root)
+    {
+        // Code
+        if (root == null)
+            return new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+        while (!stack.isEmpty() || current != null) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.data);
+            current = current.right;
+        }
+        return result;
+    }
+    
+}
