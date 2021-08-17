669ab2f8b1e0e1f7019e6498e1be0b5c77ca0db3
Rohith V <36880893+Rohithv07@users.noreply.github.com>
2021-08-17 15:22:53 +0530

    Update MagicTreeNodes.java

--- Daily Byte/MagicTreeNodes.java
+++ Daily Byte/MagicTreeNodes.java
@@ -16,67 +16,39 @@ Ex: Given the following binary tree…
  8   7, return 4 (5, 8, 7, and 9 are all magic nodes).
  */
 
- import java.util.*;
-
- public class MagicTreeNodes {
-
- 	private static int magic = 1;
- 	public static int magicNode(TreeNode root) {
- 		if (root == null)
- 			return 0;
- 		int currentMax = root.val;
- 		int previousMax = root.val;
- 		dfs(root.left, currentMax, previousMax);
- 		dfs(root.right, currentMax, previousMax);
- 		return magic;
- 	}
-
- 	public static void dfs(TreeNode node, int currentMax, int previousMax) {
- 		if (node == null) {
- 			return;
- 		}
- 		int currentNodeValue = node.val;
- 		if (currentMax < currentNodeValue && node.left == null && node.right == null) {
- 			magic++;
- 		}
- 		else if (currentNodeValue > currentMax) {
- 			magic++;
- 			previousMax = currentMax;
- 			currentMax = currentNodeValue;
- 		}
- 		dfs(node.left, currentMax, previousMax);
- 		dfs(node.right, currentMax, previousMax);
- 	}
-
- 	public static void main(String [] args) {
- 		// TreeNode root1 = new TreeNode(5);
- 		// root1.left = new TreeNode(4);
- 		// root1.right = new TreeNode(9);
- 		// root1.left.left = new TreeNode(8);
- 		// root1.left.right = new TreeNode(7);
- 		// System.out.println(magicNode(root1));
-
- 		TreeNode root2 = new TreeNode(5);
- 		root2.left = new TreeNode(9);
- 		root2.right = new TreeNode(7);
- 		root2.left.left = new TreeNode(6);
- 		root2.left.right = new TreeNode(8);
- 		System.out.println(magicNode(root2));
- 	}
- }
-
-
- class TreeNode {
- 	int val;
- 	TreeNode left;
- 	TreeNode right;
- 	public TreeNode(){}
- 	public TreeNode(int val) {
- 		this.val = val;
- 	}
- 	public TreeNode(int val, TreeNode left, TreeNode right) {
- 		this.val = val;
- 		this.right = right;
- 		this.left = left;
- 	}
- }
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+        int max = Integer.MIN_VALUE;
+        return dfs(root, max);
+    }
+    
+    public int dfs(TreeNode node, int max) {
+        if (node == null)
+            return 0;
+        int result = 0;
+        if (node.val >= max) {
+            result = 1;
+        }
+        else {
+            result = 0;
+        }
+        result += dfs(node.left, Math.max(max, node.val));
+        result += dfs(node.right, Math.max(max, node.val));
+        return result;
+    }
+}
