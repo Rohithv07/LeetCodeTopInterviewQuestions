523026d6c0d96e8ed3ca273c8821281ddcd58fcc
Rohithv07 <rohithv63@gmail.com>
2021-08-17 15:27:20 +0530

    Leetcode day 17 problem java

---
+++ AugustDailyChallenge2021/17_Count_Good_Nodes_In_Binary_Tree.java
@@ -0,0 +1,76 @@
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+Return the number of good nodes in the binary tree.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+Example 2:
+
+
+
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+Example 3:
+
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+ 
+
+Constraints:
+
+The number of nodes in the binary tree is in the range [1, 10^5].
+Each node's value is between [-10^4, 10^4].
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+        int max = Integer.MIN_VALUE;
+        return dfs(root, max);
+    }
+    
+    public int dfs(TreeNode node, int max) {
+        if (node == null)
+            return 0;
+        int result = 0;
+        if (node.val >= max) {
+            result = 1;
+        }
+        else {
+            result = 0;
+        }
+        result += dfs(node.left, Math.max(max, node.val));
+        result += dfs(node.right, Math.max(max, node.val));
+        return result;
+    }
+}
