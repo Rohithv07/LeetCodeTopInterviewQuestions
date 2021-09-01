c6ced431edf2c35cb50d42e665b39cc9afc92e13
Rohithv07 <rohithv63@gmail.com>
2021-09-01 23:47:00 +0530

    GfG problem asked in amazon, goldman, samsung in java

--- Codesignal and Geekforgeeks Combined/FindMissingAndRepeating.java
+++ Codesignal and Geekforgeeks Combined/FindMissingAndRepeating.java
@@ -26,6 +26,61 @@ Constraints:
 1 ≤ N ≤ 105
 1 ≤ Arr[i] ≤ N
 
+// using xor
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        // code here
+        int [] result = new int [2];
+        int xor = 0;
+        int setBit1Group = 0;
+        int setBit0Group = 0;
+        // first find the xor from the arr
+        for (int i=0; i<n; i++) {
+            xor ^= arr[i];
+        }
+        // now use this xor and continue xor with 1 to n
+        for (int i=1; i<=n; i++) {
+            xor ^= i;
+        }
+        // find the rightmost setbit
+        int setBit = xor & ~(xor - 1);
+        // now make two groups from the arr where the rightmost bit is 0 or 1
+        // comparing rightmost set bit of xor1 with the bit at the same position in each element.
+        for (int i=0; i<n; i++) {
+            if ((arr[i] & setBit) != 0) {
+                setBit1Group ^= arr[i];
+            }
+            else {
+                setBit0Group ^= arr[i];
+            }
+        }
+        // same way do this from i = 1 to n inclusive
+        for (int i=1; i<=n; i++) {
+            if ((i & setBit) != 0) {
+                setBit1Group ^= i;
+            }
+            else {
+                setBit0Group ^= i;
+            }
+        }
+        // we have either the value in setBit1 or setBit0 as repeating and missing
+        // we can make sure by traversing oncemore
+        for (int i=0; i<n; i++) {
+            if (arr[i] == setBit1Group) {
+                result[0] = setBit1Group;
+                result[1] = setBit0Group;
+                break;
+            }
+            else if (arr[i] == setBit0Group) {
+                result[0] = setBit0Group;
+                result[1] = setBit1Group;
+                break;
+            }
+        }
+        return result;
+    }
+}
 
 
 class Solve {
