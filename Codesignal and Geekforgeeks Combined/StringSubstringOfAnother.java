b57eac06d821f7742c50429f0c03e635668027e2
Rohithv07 <rohithv63@gmail.com>
2021-09-10 17:06:20 +0530

    Problem asked in zoho in java

---
+++ Codesignal and Geekforgeeks Combined/StringSubstringOfAnother.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class StringSubstringOfAnother {
+
+
+	public static boolean isSubstring(String ss1, String ss2) {
+		boolean matched = false;
+		int n1=ss1.length(),n2=ss2.length();
+		int i=0,j=0;
+		char [] s1 = ss1.toCharArray();
+		char [] s2 = ss2.toCharArray();
+		while(i<n1 && j<n2){
+			boolean changed=false;
+			while(s2[j]=='*' && i<n1 && j+1<n2 && s1[i]!=s2[j+1]){
+				i++;
+				changed = true;
+			}
+			if(changed) j++;
+			while(s1[i]!=s2[j]) i++;
+			while(i<n1 && j<n2 && s1[i]==s2[j]){
+				i++;
+				j++;
+			}
+			if(j==n2){
+				matched=true;
+				break;
+			}
+		}
+		if(matched) return true;
+		return false;
+		}
+
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		String s2 = sc.next();
+		System.out.println(isSubstring(s1, s2));
+	}
+}
