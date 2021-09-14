f3f60dd29642b86f0977f5cf2636dbf85250ffd8
Rohithv07 <rohithv63@gmail.com>
2021-09-14 23:15:07 +0530

    GfG problem asked in interviews in java

---
+++ Codesignal and Geekforgeeks Combined/Oracle/RootToLeafPathSum.java
@@ -0,0 +1,74 @@
+Given a binary tree and an integer S, check whether there is root to leaf path with its sum as S.
+
+Example 1:
+
+Input:
+Tree = 
+            1
+          /   \
+        2      3
+S = 2
+
+Output: 0
+
+Explanation:
+There is no root to leaf path with sum 2.
+Example 2:
+
+Input:
+Tree = 
+            1
+          /   \
+        2      3
+S = 4
+
+Output: 1
+
+Explanation:
+The sum of path from leaf node 3 to root 1 is 4.
+
+Your Task:  
+You dont need to read input or print anything. Complete the function hasPathSum() which takes root node and target sum S as input parameter and returns true if path exists otherwise it returns false.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(height of tree)
+
+Constraints:
+1 ≤ N ≤ 10^4
+1 ≤ S ≤ 10^6
+
+
+/*
+// A Binary Tree node
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+class Solution {
+    /*you are required to complete this function */
+    boolean hasPathSum(Node root, int S) {
+        // Your code here
+        if (root == null)
+            return false;
+        return dfs(root, S);
+    }
+    
+    boolean dfs(Node node, int S) {
+        if (node == null) {
+            return false;
+        }
+        if (node.left == null && node.right == null && S - node.data == 0) {
+            return true;
+        }
+        return dfs(node.left, S - node.data) || dfs(node.right, S - node.data);
+    }
+}
