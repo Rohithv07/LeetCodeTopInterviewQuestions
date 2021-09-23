7a4ffe2b79ee16a3386abc340ed896b33b510306
Rohithv07 <rohithv63@gmail.com>
2021-09-23 19:55:12 +0530

    Leetcode day 23 problem java

---
+++ SeptemberDailyChallenge2021/23_Break_A_Palindrome.java
@@ -0,0 +1,58 @@
+Given a palindromic string of lowercase English letters palindrome, replace exactly one character with any lowercase English letter so that the resulting string is not a palindrome and that it is the lexicographically smallest one possible.
+
+Return the resulting string. If there is no way to replace a character to make it not a palindrome, return an empty string.
+
+A string a is lexicographically smaller than a string b (of the same length) if in the first position where a and b differ, a has a character strictly smaller than the corresponding character in b. For example, "abcc" is lexicographically smaller than "abcd" because the first position they differ is at the fourth character, and 'c' is smaller than 'd'.
+
+ 
+
+Example 1:
+
+Input: palindrome = "abccba"
+Output: "aaccba"
+Explanation: There are many ways to make "abccba" not a palindrome, such as "zbccba", "aaccba", and "abacba".
+Of all the ways, "aaccba" is the lexicographically smallest.
+Example 2:
+
+Input: palindrome = "a"
+Output: ""
+Explanation: There is no way to replace a single character to make "a" not a palindrome, so return an empty string.
+Example 3:
+
+Input: palindrome = "aa"
+Output: "ab"
+Example 4:
+
+Input: palindrome = "aba"
+Output: "abb"
+ 
+
+Constraints:
+
+1 <= palindrome.length <= 1000
+palindrome consists of only lowercase English letters.
+
+
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        if (palindrome == null || palindrome.length() == 0) {
+            return "";
+        }
+        if (palindrome.length() == 1) {
+            return "";
+        }
+        int length = palindrome.length();
+        char [] currentString = palindrome.toCharArray();
+        for (int i=0; i<length/2; i++) {
+            char current = currentString[i];
+            if (current != 'a') {
+                // replace any non a with a at the beginning
+                currentString[i] = 'a';
+                return new String(currentString);
+            }
+        }
+        // if we have only a, then replace the last with b
+        currentString[length - 1] = 'b';
+        return new String(currentString);
+    }
+}
