88c3b9c33ba35d679024bc99c9f842e3d9f6d0d0
Rohithv07 <rohithv63@gmail.com>
2021-10-03 16:55:42 +0530

    Leetcode day 2 problem java

---
+++ OctoberDailyChallenge2021/02_Jump_Game.java
@@ -0,0 +1,39 @@
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+        int length = nums.length;
+        int canReach = 0;
+        for (int i=0; i<=canReach; i++) {
+            if (i == length - 1) {
+                return true;
+            }
+            canReach = Math.max(canReach, i + nums[i]);
+        }
+        return false;
+    }
+}
