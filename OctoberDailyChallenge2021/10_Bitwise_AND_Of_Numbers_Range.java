c09df689fd56b55c1dd9166beec5be6e6f98cb50
Rohithv07 <rohithv63@gmail.com>
2021-10-10 12:51:08 +0530

    Leetcode day 10 problem java

---
+++ OctoberDailyChallenge2021/10_Bitwise_AND_Of_Numbers_Range.java
@@ -0,0 +1,33 @@
+Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
+ 
+
+Example 1:
+
+Input: left = 5, right = 7
+Output: 4
+Example 2:
+
+Input: left = 0, right = 0
+Output: 0
+Example 3:
+
+Input: left = 1, right = 2147483647
+Output: 0
+ 
+
+Constraints:
+
+0 <= left <= right <= 231 - 1
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int count = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            count++;
+        }
+        return left << count;
+    }
+}
