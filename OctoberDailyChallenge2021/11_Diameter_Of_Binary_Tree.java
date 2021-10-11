7f5b26488f2372a90464df79d2d9194a02e73191
Rohithv07 <rohithv63@gmail.com>
2021-10-11 12:58:54 +0530

    Leetcode day 11 problem java

---
+++ OctoberDailyChallenge2021/11_Diameter_Of_Binary_Tree.java
@@ -0,0 +1,61 @@
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+Example 2:
+
+Input: root = [1,2]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-100 <= Node.val <= 100
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        findTheMaxHeight(root);
+        return max;
+    }
+    
+    public int findTheMaxHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = findTheMaxHeight(node.left);
+        int rightHeight = findTheMaxHeight(node.right);
+        max = Math.max(max, leftHeight + rightHeight);
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
