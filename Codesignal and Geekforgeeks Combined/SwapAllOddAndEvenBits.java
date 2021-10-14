f3981e6cccf367080cbbdabbbddd7f51743a252b
Rohithv07 <rohithv63@gmail.com>
2021-10-14 15:11:44 +0530

    GfG problem asked in interviews in java

--- Codesignal and Geekforgeeks Combined/SwapAllOddAndEvenBits.java
+++ Codesignal and Geekforgeeks Combined/SwapAllOddAndEvenBits.java
@@ -51,3 +51,28 @@ class Solution
 	}
     
 }
+
+class Solution
+{
+    //Function to swap odd and even bits.
+    public int swapBits(int n) 
+    {
+	    //0xAAAAAAAA means 10101010101010101010101010101010 in binary
+	    //we get all even bits of n.
+	    int ev=n & 0xAAAAAAAA;
+	    //0x55555555 means 01010101010101010101010101010101 in binary.
+	    //we get all odd bits of n.
+	    int od=n & 0x55555555; 
+	    
+	    //right Shifting the even bits obtained previously.
+	    ev>>=1;
+	    //left Shifting the even bits obtained previously.
+	    od<<=1;
+	    
+	    //doing bitwise OR of even and odd bits obtained and
+	    //returning the final result.
+	    return (ev | od); 
+	    
+	}
+    
+}
