635c2c4f361cd825f2b38f5d7449a1f3919c39ef
Rohithv07 <rohithv63@gmail.com>
2021-10-28 19:34:26 +0530

    Leetcode day 28 problem java

---
+++ OctoberDailyChallenge2021/28_3Sum.java
@@ -0,0 +1,64 @@
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+ 
+
+Constraints:
+
+0 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> possibleList = new ArrayList<>();
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int i=0; i+2<length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = length - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == 0) {
+                    possibleList.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left-1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+                else if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                }
+                else {
+                    left++;
+                }
+            }
+        }
+        return possibleList;
+    }
+}
