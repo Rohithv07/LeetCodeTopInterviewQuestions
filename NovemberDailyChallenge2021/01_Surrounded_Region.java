cdc03b37b5ae7de4f6afc8f346b5b589077c08ca
Rohithv07 <rohithv63@gmail.com>
2021-11-01 10:26:26 +0530

    Leetcode day 1 problem java

---
+++ NovemberDailyChallenge2021/01_Surrounded_Region.java
@@ -0,0 +1,75 @@
+Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+ 
+
+Example 1:
+
+
+Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+Explanation: Surrounded regions should not be on the border, which means that any 'O' on the border of the board are not flipped to 'X'. Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'. Two cells are connected if they are adjacent cells connected horizontally or vertically.
+Example 2:
+
+Input: board = [["X"]]
+Output: [["X"]]
+ 
+
+Constraints:
+
+m == board.length
+n == board[i].length
+1 <= m, n <= 200
+board[i][j] is 'X' or 'O'.
+
+
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        int row = board.length;
+        int col = board[0].length;
+        if (row == 1 || col == 1) {
+            return;
+        }
+        for (int i=0; i<row; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][col - 1] == 'O') {
+                dfs(board, i, col - 1);
+            }
+        }
+        for (int j=1; j<col-1; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+            if (board[row - 1][j] == 'O') {
+                dfs(board, row - 1, j);
+            }
+        }
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                else if (board[i][j] == '&') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    
+    private void dfs(char [][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = '&';
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
+        dfs(board, i, j-1);
+        dfs(board, i, j+1);
+    }
+}
