e3cf942d1696fcc271f05e4d4bc5985d86787fb5
Rohithv07 <rohithv63@gmail.com>
2021-11-02 16:42:19 +0530

    Leetcode day 2 problem java

---
+++ NovemberDailyChallenge2021/02_Unique_Paths_III.java
@@ -0,0 +1,100 @@
+You are given an m x n integer array grid where grid[i][j] could be:
+
+1 representing the starting square. There is exactly one starting square.
+2 representing the ending square. There is exactly one ending square.
+0 representing empty squares we can walk over.
+-1 representing obstacles that we cannot walk over.
+Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+Output: 2
+Explanation: We have the following two paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+Example 2:
+
+
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+Output: 4
+Explanation: We have the following four paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+Example 3:
+
+
+Input: grid = [[0,1],[2,0]]
+Output: 0
+Explanation: There is no path that walks over every empty square exactly once.
+Note that the starting and ending square can be anywhere in the grid.
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 20
+1 <= m * n <= 20
+-1 <= grid[i][j] <= 2
+There is exactly one starting cell and one ending cell.
+
+
+
+class Solution {
+    public int uniquePathsIII(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        int emptySquare = 0;
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (grid[i][j] == 0) {
+                    emptySquare++;
+                }
+            }
+        }
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (grid[i][j] == 1) {
+                    return helper(grid, i+1, j, emptySquare) +
+                        helper(grid, i-1, j, emptySquare) + 
+                        helper(grid, i, j-1, emptySquare) +
+                        helper(grid, i, j+1, emptySquare);
+                }
+            }
+        }
+        return 0;
+    }
+    
+    private int helper(int [][] grid, int i, int j, int emptySquare) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == -1 || grid[i][j] == 1) {
+            return 0;
+        }
+        if (grid[i][j] == 2) {
+            if (emptySquare == 0) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+        if (emptySquare == 0) {
+            return 0;
+        }
+        grid[i][j] = 1;
+        int path1 = helper(grid, i+1, j, emptySquare-1);
+        int path2 = helper(grid, i-1, j, emptySquare-1);
+        int path3 = helper(grid, i, j-1, emptySquare-1);
+        int path4 = helper(grid, i, j+1, emptySquare-1);
+        grid[i][j] = 0;
+        return path1 + path2 + path3 + path4;
+    }
+}
