a0fd5451301fdcf87be87b1e7e7fef0d4a76c6bf
Rohithv07 <rohithv63@gmail.com>
2021-11-15 10:45:15 +0530

    GfG problem asked in adobe, amazon, microsoft, google, goldman in java

---
+++ Codesignal and Geekforgeeks Combined/MirrorTree.java
@@ -0,0 +1,81 @@
+Given a Binary Tree, convert it into its mirror.
+MirrorTree1            
+
+Example 1:
+
+Input:
+      1
+    /  \
+   2    3
+Output: 2 1 3
+Explanation: The tree is
+   1    (mirror)  1
+ /  \    =>      /  \
+3    2          2    3
+The inorder of mirror is 2 1 3
+Example 2:
+
+Input:
+      10
+     /  \
+    20   30
+   /  \
+  40  60
+Output: 30 10 60 20 40
+Explanation: The tree is
+      10               10
+    /    \  (mirror) /    \
+   20    30    =>   30    20
+  /  \                   /   \
+ 40  60                 60   40
+The inroder traversal of mirror is
+30 10 60 20 40.
+Your Task:
+Just complete the function mirror() that takes node as paramter  and convert it into its mirror. The printing is done by the driver code only.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 ≤ Number of nodes ≤ 105
+1 ≤ Data of a node ≤ 105
+
+
+// User function Template for Java
+
+// function Template for Java
+
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        // Your code here
+        if (node == null) {
+            return;
+        }
+        node = invert(node);
+    }
+    
+    Node invert(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Node left = invert(node.left);
+        Node right = invert(node.right);
+        node.left = right;
+        node.right = left;
+        return node;
+    }
+}
