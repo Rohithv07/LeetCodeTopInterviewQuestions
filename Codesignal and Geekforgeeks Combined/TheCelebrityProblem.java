849f806565fe36bde2f0de35e5d484b16c5227c4
Rohithv07 <rohithv63@gmail.com>
2021-12-18 17:32:21 +0530

    GfG problem asked in interviews in java

--- Codesignal and Geekforgeeks Combined/TheCelebrityProblem.java
+++ Codesignal and Geekforgeeks Combined/TheCelebrityProblem.java
@@ -1,3 +1,7 @@
+// https://www.youtube.com/watch?v=Z5AEc12ieOs&t=1382s
+
+
+
 A celebrity is a person who is known to all but does not know anyone at a party. If you go to a party of N people, find if there is a celebrity in the party or not.
 A square NxN matrix M[][] is used to represent people at the party such that if an element of row i and column j  is set to 1 it means ith person knows jth person. Here M[i][i] will always be 0.
 Note: Follow 0 based indexing.
