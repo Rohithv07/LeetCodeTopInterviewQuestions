ba1462fa26d43cad97e0d0d9d90e01cda34ca07c
Rohithv07 <rohithv63@gmail.com>
2021-12-21 09:30:06 +0530

    Leetcode day 21 problem java

---
+++ DecemberDailyChallenge2021/21_Power_Of_Two.java
@@ -0,0 +1,43 @@
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: n = 3
+Output: false
+ 
+
+Constraints:
+
+-231 <= n <= 231 - 1
+ 
+
+Follow up: Could you solve it without loops/recursion?
+
+
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+       return (n & (n - 1)) == 0;
+        //return (Math.log10(n) / Math.log10(2)) % 1.0 == 0;
+    }
+}
