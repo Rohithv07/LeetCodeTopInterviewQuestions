1b77284a3605ae88cc23e971f529fdcebd649dc5
Rohithv07 <rohithv63@gmail.com>
2021-12-30 10:29:05 +0530

    Leetcode day 30 problem java

---
+++ DecemberDailyChallenge2021/30_Smallest_Integer_Divisible_By_K.java
@@ -0,0 +1,44 @@
+Given a positive integer k, you need to find the length of the smallest positive integer n such that n is divisible by k, and n only contains the digit 1.
+
+Return the length of n. If there is no such n, return -1.
+
+Note: n may not fit in a 64-bit signed integer.
+
+ 
+
+Example 1:
+
+Input: k = 1
+Output: 1
+Explanation: The smallest answer is n = 1, which has length 1.
+Example 2:
+
+Input: k = 2
+Output: -1
+Explanation: There is no such positive integer n divisible by 2.
+Example 3:
+
+Input: k = 3
+Output: 3
+Explanation: The smallest answer is n = 111, which has length 3.
+ 
+
+Constraints:
+
+1 <= k <= 105
+
+
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        if (k % 2 == 0 || k % 5 == 0) {
+            return -1;
+        }
+        int runningNumber = 1;
+        int length = 1;
+        while (runningNumber % k != 0) {
+            runningNumber = (runningNumber % k) * 10 + 1; // we only consider the n with 1's
+            length += 1;
+        }
+        return length;
+    }
+}
