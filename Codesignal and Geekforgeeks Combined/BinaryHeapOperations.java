ecc1b34bd20c9c11072c59e69694810bacc079fb
Rohithv07 <rohithv63@gmail.com>
2021-12-30 10:14:23 +0530

    GfG problem asked in amazon, microsoft, walmart in java

---
+++ Codesignal and Geekforgeeks Combined/BinaryHeapOperations.java
@@ -0,0 +1,158 @@
+A binary heap is a Binary Tree with the following properties:
+1) It’s a complete tree (All levels are completely filled except possibly the last level and the last level has all keys as left as possible). This property of Binary Heap makes them suitable to be stored in an array.
+
+2) A Binary Heap is either Min Heap or Max Heap. In a Min Binary Heap, the key at the root must be minimum among all keys present in Binary Heap. The same property must be recursively true for all nodes in Binary Tree. Max Binary Heap is similar to MinHeap.
+
+You are given an empty Binary Min Heap and some queries and your task is to implement the three methods insertKey,  deleteKey,  and extractMin on the Binary Min Heap and call them as per the query given below:
+1) 1  x  (a query of this type means to insert an element in the min-heap with value x )
+2) 2  x  (a query of this type means to remove an element at position x from the min-heap)
+3) 3  (a query like this removes the min element from the min-heap and prints it ).
+
+Example 1:
+
+Input:
+Q = 7
+Queries:
+insertKey(4)
+insertKey(2)
+extractMin()
+insertKey(6)
+deleteKey(0)
+extractMin()
+extractMin()
+Output: 2 6 - 1
+Explanation: In the first test case for
+query 
+insertKey(4) the heap will have  {4}  
+insertKey(2) the heap will be {2 4}
+extractMin() removes min element from 
+             heap ie 2 and prints it
+             now heap is {4} 
+insertKey(6) inserts 6 to heap now heap
+             is {4 6}
+deleteKey(0) delete element at position 0
+             of the heap,now heap is {6}
+extractMin() remove min element from heap
+             ie 6 and prints it  now the
+             heap is empty
+extractMin() since the heap is empty thus
+             no min element exist so -1
+             is printed.
+Example 2:
+
+Input:
+Q = 5
+Queries:
+insertKey(8)
+insertKey(9)
+deleteKey(1)
+extractMin()
+extractMin()
+Output: 8 -1
+Your Task:
+You are required to complete the 3 methods insertKey() which take one argument the value to be inserted, deleteKey() which takes one argument the position from where the element is to be deleted and extractMin() which returns the minimum element in the heap(-1 if the heap is empty)
+
+Expected Time Complexity: O(Q*Log(size of Heap) ).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= Q <= 104
+1 <= x <= 104
+
+
+
+
+
+
+// User function Template for Java
+
+
+class MinHeap {
+    int[] harr;
+    int capacity, heap_size;
+    MinHeap(int cap) {
+        heap_size = 0;
+        capacity = cap;
+        harr = new int[cap];
+    }
+    int parent(int i) { return (i - 1) / 2; }
+    int left(int i) { return (2 * i + 1); }
+    int right(int i) { return (2 * i + 2); }
+
+    //Function to extract minimum value in heap and then to store 
+    //next minimum value at first index.
+    int extractMin()
+    {
+        // Your code here.
+        if (heap_size == 0) {
+            return -1;
+        }
+        if (heap_size == 1) {
+            heap_size--;
+            return harr[0];
+        }
+        swap(harr, 0, heap_size - 1);
+        heap_size--;
+        MinHeapify(0);
+        return harr[heap_size];
+    }
+
+    //Function to insert a value in Heap.
+    void insertKey(int k) 
+    {
+        // Your code here.
+        heap_size++;
+        int i = heap_size - 1;
+        harr[i] = k;
+        while (i != 0 && harr[parent(i)] > harr[i]) {
+            swap(harr, i, parent(i));
+            i = parent(i);
+        }
+    }
+
+    //Function to delete a key at ith index.
+    void deleteKey(int i) 
+    {
+        // Your code here.
+        if (heap_size <= 0 || heap_size <= i) {
+            return;
+        }
+        decreaseKey(i, Integer.MIN_VALUE);
+        extractMin();
+    }
+
+    //Function to change value at ith index and store that value at first index.
+    void decreaseKey(int i, int new_val) 
+    {
+        harr[i] = new_val;
+        while (i != 0 && harr[parent(i)] > harr[i]) {
+            int temp = harr[i];
+            harr[i] = harr[parent(i)];
+            harr[parent(i)] = temp;
+            i = parent(i);
+        }
+    }
+    
+    void swap(int [] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    /* You may call below MinHeapify function in
+      above codes. Please do not delete this code
+      if you are not writing your own MinHeapify */
+    void MinHeapify(int i) {
+        int l = left(i);
+        int r = right(i);
+        int smallest = i;
+        if (l < heap_size && harr[l] < harr[i]) smallest = l;
+        if (r < heap_size && harr[r] < harr[smallest]) smallest = r;
+        if (smallest != i) {
+            int temp = harr[i];
+            harr[i] = harr[smallest];
+            harr[smallest] = temp;
+            MinHeapify(smallest);
+        }
+    }
+}
