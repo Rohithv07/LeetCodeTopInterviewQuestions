121c954ff3f4fd137111186e1e84d4ce555e64cd
Rohithv07 <rohithv63@gmail.com>
2022-01-01 17:39:57 +0530

    GfG problem asked in amazon, microsoft in java

--- Codesignal and Geekforgeeks Combined/BridgeEdgeInAGraph.java
+++ Codesignal and Geekforgeeks Combined/BridgeEdgeInAGraph.java
@@ -45,6 +45,8 @@ Constraints:
 1 ≤ V,E ≤ 105
 0 ≤ c, d ≤ V-1
 
+
+// solution by altering the graph
 class Solution
 {
     //Function to find if the given edge is a bridge in graph.
@@ -66,5 +68,56 @@ class Solution
                 dfs(adj, visited, child);
             }
         }
+    }
+}
+
+
+// our standard answer for the bridge problem : https://youtu.be/2rjZH0-2lhk
+
+
+class Solution
+{
+    //Function to find if the given edge is a bridge in graph.
+    static int isBridge(int v, ArrayList<ArrayList<Integer>> adj,int c,int d)
+    {
+        // code here
+        boolean [] visited = new boolean [v];
+        int [] timeOfInsertion = new int [v];
+        int [] lowestTime = new int [v];
+        int timer = 0;
+        for (int i=0; i<v; i++) {
+            if (!visited[i]) {
+                if (isBridgeFoundAtCD(i, -1, visited, timeOfInsertion, lowestTime, adj, timer, c, d)) {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+    
+    static boolean isBridgeFoundAtCD(int node, int parent, boolean [] visited, int [] timeOfInsertion, int [] lowestTime, ArrayList<ArrayList<Integer>>adj, int timer, int c, int d) {
+        visited[node] = true;
+        timeOfInsertion[node] = lowestTime[node] = timer++;
+        ArrayList<Integer> children =adj.get(node);
+        for (Integer child : children) {
+            if (child == parent) {
+                continue;
+            }
+            if (!visited[child]) {
+                if (isBridgeFoundAtCD(child, node, visited, timeOfInsertion, lowestTime, adj, timer, c, d)) {
+                    return true;
+                }
+                lowestTime[node] = Math.min(lowestTime[node], lowestTime[child]);
+                if (lowestTime[child] > timeOfInsertion[node]) {
+                    if ((child == c && node == d) || (child == d && node == c)) {
+                        return true;
+                    }
+                }
+            }
+            else {
+                lowestTime[node] = Math.min(lowestTime[node], timeOfInsertion[child]);
+            }
+        }
+        return false;
     }
 }
