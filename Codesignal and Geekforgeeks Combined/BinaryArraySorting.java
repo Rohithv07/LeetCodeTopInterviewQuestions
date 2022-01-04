342a4a31d95cf5be45a03909542992989079c572
Rohithv07 <rohithv63@gmail.com>
2022-01-05 00:18:14 +0530

    GfG problem asked in paytm, intuit in java

--- Codesignal and Geekforgeeks Combined/BinaryArraySorting.java
+++ Codesignal and Geekforgeeks Combined/BinaryArraySorting.java
@@ -48,3 +48,71 @@ class Solution
 	    return arr;
 	} 
 } 
+
+
+
+class Solution
+{
+    //Function to sort the binary array.
+    static void binSort(int a[], int n)
+    {
+        // add your code here
+        
+        
+        /**************
+        * No need to print the array
+        * ************/
+        int movingPointer = 0;
+        int trackZero = 0;
+        while (movingPointer < n) {
+            if (a[movingPointer] == 0) {
+                swap(a, trackZero, movingPointer);
+                trackZero++;
+            }
+            movingPointer++;
+        }
+    }
+    
+    static void swap(int [] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
+
+class Solution
+{
+    //Function to sort the binary array.
+    static void binSort(int A[], int N)
+    {
+        // add your code here
+        
+        
+        /**************
+        * No need to print the array
+        * ************/
+        int i = 0;
+        int j = N - 1;
+        while (i < j) {
+            if (A[i] == 1 && A[j] == 0) {
+                swap(A, i, j);
+                i++;
+                j--;
+            }
+            if (A[i] == 0) {
+                i++;
+            }
+            if (A[j] == 1) {
+                j--;
+            }
+        }
+    }
+    
+    private static void swap(int [] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
+
+
