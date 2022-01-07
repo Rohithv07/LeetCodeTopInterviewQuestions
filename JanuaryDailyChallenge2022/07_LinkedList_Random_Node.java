eea039a24dce7fa37784c991bf613b51b681a00f
Rohithv07 <rohithv63@gmail.com>
2022-01-07 10:03:57 +0530

    Leetcode day 7 problem java

---
+++ JanuaryDailyChallenge2022/07_LinkedList_Random_Node.java
@@ -0,0 +1,78 @@
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Implement the Solution class:
+
+Solution(ListNode head) Initializes the object with the integer array nums.
+int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be choosen.
+ 
+
+Example 1:
+
+
+Input
+["Solution", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom"]
+[[[1, 2, 3]], [], [], [], [], []]
+Output
+[null, 1, 3, 2, 2, 3]
+
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.getRandom(); // return 1
+solution.getRandom(); // return 3
+solution.getRandom(); // return 2
+solution.getRandom(); // return 2
+solution.getRandom(); // return 3
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+ 
+
+Constraints:
+
+The number of nodes in the linked list will be in the range [1, 104].
+-104 <= Node.val <= 104
+At most 104 calls will be made to getRandom.
+ 
+
+Follow up:
+
+What if the linked list is extremely large and its length is unknown to you?
+Could you solve this efficiently without using extra space?
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    private ListNode head;
+    
+    public Solution(ListNode head) {
+        this.head = head;
+    }
+    
+    public int getRandom() {
+        int scope = 1;
+        ListNode current = head;
+        int random = 0;
+        while (current != null) {
+            if (Math.random() < 1.0 / scope) {
+                random = current.val;
+            }
+            scope += 1;
+            current = current.next;
+        }
+        return random;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
