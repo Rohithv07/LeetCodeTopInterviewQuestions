79017f5c35374b7cd8d698fba2450c38faa36ae7
Rohithv07 <rohithv63@gmail.com>
2022-02-05 10:19:19 +0530

    Leetcode day 5 problem java

---
+++ FebruaryDailyChallenge2022/05_Merge_K_Sorted_Lists.java
@@ -0,0 +1,106 @@
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 10^4
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] is sorted in ascending order.
+The sum of lists[i].length won't exceed 10^4.
+
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        List<Integer> list = new ArrayList<>();
+        for (ListNode l : lists) {
+            while (l != null) {
+                list.add(l.val);
+                l = l.next;
+            }
+        }
+        Collections.sort(list);
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        for (int num : list) {
+            current.next = new ListNode(num);
+            current = current.next;
+        }
+        return result.next;
+    }
+}
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (ListNode head : lists) {
+            while (head != null) {
+                minHeap.add(head.val);
+                head = head.next;
+            }
+        }
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        while (!minHeap.isEmpty()) {
+            current.next = new ListNode(minHeap.remove());
+            current = current.next;
+        }
+        return result.next;
+    }
+}
