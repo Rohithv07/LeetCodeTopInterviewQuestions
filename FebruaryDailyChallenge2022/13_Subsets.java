39f77a1d2457b46f6cb51343dbccb8c44ae9e8b1
Rohithv07 <rohithv63@gmail.com>
2022-02-13 10:05:46 +0530

    Leetcode day 13 problem java

---
+++ FebruaryDailyChallenge2022/13_Subsets.java
@@ -0,0 +1,42 @@
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        backtrack(result, current, nums, 0);
+        return result;
+    }
+    
+    public void backtrack(List<List<Integer>> result, List<Integer> current, int [] nums, int position) {
+        result.add(new ArrayList<>(current));
+        for (int i = position; i < nums.length; i++) {
+            current.add(nums[i]);
+            backtrack(result, current, nums, i + 1);
+            current.remove(current.size() - 1);
+        }
+    }
+}
