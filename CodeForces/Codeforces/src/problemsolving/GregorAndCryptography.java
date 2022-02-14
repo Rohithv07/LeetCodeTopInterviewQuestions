890b38a5e23a3844480b24cb25a9fddc7b970605
Rohithv07 <rohithv63@gmail.com>
2022-02-14 17:32:30 +0530

    Codeforces 1549A problem java

---
+++ CodeForces/Codeforces/src/problemsolving/GregorAndCryptography.java
@@ -0,0 +1,16 @@
+package problemsolving;
+
+import java.util.*;
+
+public class GregorAndCryptography {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		while (test-- > 0) {
+			long number = sc.nextLong();
+			System.out.println("2 " + (number - 1));
+
+		}
+	}
+}
