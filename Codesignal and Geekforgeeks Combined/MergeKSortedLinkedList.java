6dc1db436fbf5958426ecdd8fe0aa3d7fc519174
Rohithv07 <rohithv63@gmail.com>
2022-03-07 12:27:32 +0530

    GfG problem asked in amazon, microsoft, vmware in java

---
+++ Codesignal and Geekforgeeks Combined/MergeKSortedLinkedList.java
@@ -0,0 +1,87 @@
+Given K sorted linked lists of different sizes. The task is to merge them in such a way that after merging they will be a single sorted linked list.
+
+Example 1:
+
+Input:
+K = 4
+value = {{1,2,3},{4 5},{5 6},{7,8}}
+Output: 1 2 3 4 5 5 6 7 8
+Explanation:
+The test case has 4 sorted linked 
+list of size 3, 2, 2, 2
+1st    list     1 -> 2-> 3
+2nd   list      4->5
+3rd    list      5->6
+4th    list      7->8
+The merged list will be
+1->2->3->4->5->5->6->7->8.
+Example 2:
+
+Input:
+K = 3
+value = {{1,3},{4,5,6},{8}}
+Output: 1 3 4 5 6 8
+Explanation:
+The test case has 3 sorted linked
+list of size 2, 3, 1.
+1st list 1 -> 3
+2nd list 4 -> 5 -> 6
+3rd list 8
+The merged list will be
+1->3->4->5->6->8.
+Your Task:
+The task is to complete the function mergeKList() which merges the K given lists into a sorted one. The printing is done automatically by the driver code.
+
+Expected Time Complexity: O(nk Logk)
+Expected Auxiliary Space: O(k)
+Note: n is the maximum size of all the k link list
+
+Constraints
+1 <= K <= 103
+
+
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// a is an array of Nodes of the heads of linked lists
+// and N is size of array a
+
+
+class Solution
+{
+    //Function to merge K sorted linked list.
+    Node mergeKList(Node[]arr,int k)
+    {
+        //Add your code here.
+        if (arr == null) {
+            return null;
+        }
+        Node result = new Node(0);
+        Node current = result;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (Node ar : arr) {
+            Node moving = ar;
+            while (moving != null) {
+                minHeap.offer(moving.data);
+                moving = moving.next;
+            }
+        }
+        while (!minHeap.isEmpty()) {
+            current.next = new Node(minHeap.poll());
+            current = current.next;
+        }
+        return result.next;
+    }
+}
