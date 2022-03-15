d6cdee2f86fc3f680ddc608315cd5773e5ca2ad4
Rohithv07 <rohithv63@gmail.com>
2022-03-15 11:09:28 +0530

    Leetcode day 15 problem in java

--- MarchDailyChallenge2022/15_Minimum_Move_To_Make_Valid_Parentheses.java
+++ MarchDailyChallenge2022/15_Minimum_Move_To_Make_Valid_Parentheses.java
@@ -40,3 +40,35 @@ class Solution {
         return result.reverse().toString();
     }
 }
+
+// using stack
+
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int length = s.length();
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                stack.push(i);
+            }
+            else if (ch == ')') {
+                if (!stack.isEmpty() && s.charAt(stack.peek()) == '(') {
+                    stack.pop();
+                }
+                else {
+                    stack.push(i);
+                }
+            }
+            sb.append(s.charAt(i));
+        }
+        while (!stack.isEmpty()) {
+            sb.deleteCharAt(stack.pop());
+        }
+        return sb.toString();
+    }
+}
