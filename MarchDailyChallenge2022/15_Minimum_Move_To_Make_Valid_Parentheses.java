f2b91b2ffc7c1ffc0f177d089cc997b5a3fdee58
Rohithv07 <rohithv63@gmail.com>
2022-03-15 10:57:52 +0530

    Leetcode day 15 problem in java

---
+++ MarchDailyChallenge2022/15_Minimum_Move_To_Make_Valid_Parentheses.java
@@ -0,0 +1,42 @@
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+
+
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int length = s.length();
+        StringBuilder sb = new StringBuilder();
+        int openBracketCount = 0;
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                openBracketCount ++;
+            }
+            else if (ch == ')') {
+                if (openBracketCount == 0) {
+                    continue;
+                }
+                openBracketCount--;
+            }
+            sb.append(ch);
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = sb.length() - 1; i >= 0; i--) {
+            if (sb.charAt(i) == '(' && openBracketCount-- > 0) {
+                continue;
+            }
+            result.append(sb.charAt(i));
+        }
+        return result.reverse().toString();
+    }
+}
