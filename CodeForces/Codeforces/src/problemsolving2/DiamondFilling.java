29753c075be45c65d95c724b1b191639d08209cf
Rohithv07 <rohithv63@gmail.com>
2022-04-04 22:40:21 +0530

    Codeforces 1339A problem in java

---
+++ CodeForces/Codeforces/src/problemsolving2/DiamondFilling.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package problemsolving2;
+
+import java.util.Scanner;
+
+/**
+ * @author rohithvazhathody
+ *
+ *
+ */
+public class DiamondFilling {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		while (test-- > 0) {
+			System.out.println(sc.next());
+		}
+	}
+}
