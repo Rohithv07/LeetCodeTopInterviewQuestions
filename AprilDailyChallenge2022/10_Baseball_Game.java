c76b39758bcb3205a178c3d727fd4c97f96864c5
Rohithv07 <rohithv63@gmail.com>
2022-04-10 10:25:04 +0530

    Leetcode day 10 problem in java

---
+++ AprilDailyChallenge2022/10_Baseball_Game.java
@@ -0,0 +1,85 @@
+You are keeping score for a baseball game with strange rules. The game consists of several rounds, where the scores of past rounds may affect future rounds' scores.
+
+At the beginning of the game, you start with an empty record. You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x - Record a new score of x.
+"+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+"D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+"C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+Return the sum of all the scores on the record.
+
+ 
+
+Example 1:
+
+Input: ops = ["5","2","C","D","+"]
+Output: 30
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"2" - Add 2 to the record, record is now [5, 2].
+"C" - Invalidate and remove the previous score, record is now [5].
+"D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+"+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+The total sum is 5 + 10 + 15 = 30.
+Example 2:
+
+Input: ops = ["5","-2","4","C","D","9","+","+"]
+Output: 27
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"-2" - Add -2 to the record, record is now [5, -2].
+"4" - Add 4 to the record, record is now [5, -2, 4].
+"C" - Invalidate and remove the previous score, record is now [5, -2].
+"D" - Add 2 * -2 = -4 to the record, record is now [5, -2, -4].
+"9" - Add 9 to the record, record is now [5, -2, -4, 9].
+"+" - Add -4 + 9 = 5 to the record, record is now [5, -2, -4, 9, 5].
+"+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
+The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
+Example 3:
+
+Input: ops = ["1"]
+Output: 1
+ 
+
+Constraints:
+
+1 <= ops.length <= 1000
+ops[i] is "C", "D", "+", or a string representing an integer in the range [-3 * 104, 3 * 104].
+For operation "+", there will always be at least two previous scores on the record.
+For operations "C" and "D", there will always be at least one previous score on the record.
+
+
+
+class Solution {
+    public int calPoints(String[] ops) {
+        if (ops == null || ops.length == 0) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int sum = 0;
+        for (String s : ops) {
+            if (s.equals("C")) {
+                stack.pop();
+            }
+            else if (s.equals("D")) {
+                int doubles = 2 * (stack.peek());
+                stack.push(doubles);
+            }
+            else if (s.equals("+")) {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+                int add = num1 + num2;
+                stack.push(num2);
+                stack.push(num1);
+                stack.push(add);
+            }
+            else {
+                stack.push(Integer.valueOf(s));
+            }
+        }
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        return sum;
+    }
+}
