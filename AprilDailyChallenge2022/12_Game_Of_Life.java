59a0c7454bd1a9bcbf72c84f59ea9fbdfed0e9e8
Rohithv07 <rohithv63@gmail.com>
2022-04-12 11:08:56 +0530

    Leetcode day 12 problem in java

--- AprilDailyChallenge2022/12_Game_Of_Life.java
+++ AprilDailyChallenge2022/12_Game_Of_Life.java
@@ -72,3 +72,54 @@ class Solution {
         return count;
     }
 }
+
+
+// this is not bfs or dfs
+
+class Solution {
+    
+    private final int [][] directions = new int [][] {{0, 1}, {1, 0}, {-1, 0}, {0, -1},
+                                                      {-1, 1}, {1, -1}, {1, 1}, {-1, -1}};
+    
+    public void gameOfLife(int[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        
+        int row = board.length;
+        int col = board[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                int live = 0;
+                for (int [] dir : directions) {
+                    int x = dir[0] + i;
+                    int y = dir[1] + j;
+                    if (x >= 0 && y >= 0 && x < row && y < col && Math.abs(board[x][y]) == 1) {
+                        live++;
+                    }
+                }
+                if (board[i][j] == 1) {
+                    if (live < 2 || live > 3) {
+                        board[i][j] = -1;
+                    }
+                }
+                else {
+                    if (live == 3) {
+                        board[i][j] = 2;
+                    }
+                }
+            }
+        }
+        // change all 2 to 1 and -1 to 0
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == 2) {
+                    board[i][j] = 1;
+                } 
+                else if (board[i][j] == -1) {
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+}
