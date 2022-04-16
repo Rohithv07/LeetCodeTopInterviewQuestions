5e2e64ecd396b1ec205afc53dc9010698161cb4d
Rohithv07 <rohithv63@gmail.com>
2022-04-16 16:20:08 +0530

    Leetcode day 16 problem in java

---
+++ AprilDailyChallenge2022/16_Convert_BST_To_Greater_Tree.java
@@ -0,0 +1,61 @@
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+
+Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+Example 2:
+
+Input: root = [0,null,1]
+Output: [1,null,1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-104 <= Node.val <= 104
+All the values in the tree are unique.
+root is guaranteed to be a valid binary search tree.
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        dfs(root, 0);
+        return root;
+    }
+    
+    public int dfs(TreeNode node, int val) {
+        if (node == null) {
+            return val;
+        }
+        node.val += dfs(node.right, val);
+        return dfs(node.left, node.val);
+    }
+}
