c5ee80f8570adfcecc0a48ed077cad64b374e825
Rohithv07 <rohithv63@gmail.com>
2022-04-17 17:17:03 +0530

    Leetcode day 17 problem in java

---
+++ AprilDailyChallenge2022/17_Increasing_Order_Search_Tree.java
@@ -0,0 +1,54 @@
+Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+Example 2:
+
+
+Input: root = [5,1,7]
+Output: [1,null,5,null,7]
+ 
+
+Constraints:
+
+The number of nodes in the given tree will be in the range [1, 100].
+0 <= Node.val <= 1000
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        return makeIncreasing(root, null);
+    }
+    
+    public TreeNode makeIncreasing(TreeNode node, TreeNode tail) {
+        if (node == null) {
+            return tail;
+        }
+        TreeNode result = makeIncreasing(node.left, node);
+        node.left = null;
+        node.right = makeIncreasing(node.right, tail);
+        return result;
+    }
+}
