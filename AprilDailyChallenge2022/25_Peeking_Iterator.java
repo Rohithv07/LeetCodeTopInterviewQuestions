74a1e32418cbda4d613d370c53c90cc12b21e1cb
Rohithv07 <rohithv63@gmail.com>
2022-04-25 20:49:19 +0530

    Leetcode day 25 problem in java

---
+++ AprilDailyChallenge2022/25_Peeking_Iterator.java
@@ -0,0 +1,79 @@
+Design an iterator that supports the peek operation on an existing iterator in addition to the hasNext and the next operations.
+
+Implement the PeekingIterator class:
+
+PeekingIterator(Iterator<int> nums) Initializes the object with the given integer iterator iterator.
+int next() Returns the next element in the array and moves the pointer to the next element.
+boolean hasNext() Returns true if there are still elements in the array.
+int peek() Returns the next element in the array without moving the pointer.
+Note: Each language may have a different implementation of the constructor and Iterator, but they all support the int next() and boolean hasNext() functions.
+
+ 
+
+Example 1:
+
+Input
+["PeekingIterator", "next", "peek", "next", "next", "hasNext"]
+[[[1, 2, 3]], [], [], [], [], []]
+Output
+[null, 1, 2, 2, 3, false]
+
+Explanation
+PeekingIterator peekingIterator = new PeekingIterator([1, 2, 3]); // [1,2,3]
+peekingIterator.next();    // return 1, the pointer moves to the next element [1,2,3].
+peekingIterator.peek();    // return 2, the pointer does not move [1,2,3].
+peekingIterator.next();    // return 2, the pointer moves to the next element [1,2,3]
+peekingIterator.next();    // return 3, the pointer moves to the next element [1,2,3]
+peekingIterator.hasNext(); // return False
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+1 <= nums[i] <= 1000
+All the calls to next and peek are valid.
+At most 1000 calls will be made to next, hasNext, and peek.
+ 
+
+Follow up: How would you extend your design to be generic and work with all types, not just integer?
+
+
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+class PeekingIterator implements Iterator<Integer> {
+    
+    private Integer next = null;
+    private Iterator<Integer> iter;
+    private boolean noSuchElement;
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    // initialize any member here.
+        iter = iterator;
+        if (iter.hasNext()) {
+            next = iter.next();
+        }
+        else {
+            noSuchElement = true;
+        }
+	    
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        return next;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+	    Integer result = next;
+        next = iter.hasNext() ? iter.next() : null;
+        return result;
+	}
+	
+	@Override
+	public boolean hasNext() {
+	    return next != null;
+	}
+}
