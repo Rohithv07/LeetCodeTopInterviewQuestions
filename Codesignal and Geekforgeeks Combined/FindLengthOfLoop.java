e6bb30c676326e0a1ff368778861fcb11f29dca4
Rohithv07 <rohithv63@gmail.com>
2022-04-29 00:30:56 +0530

    GfG problem asked in adobe, qualcomm, amazon in java

---
+++ Codesignal and Geekforgeeks Combined/FindLengthOfLoop.java
@@ -0,0 +1,81 @@
+Given a linked list of size N. The task is to complete the function countNodesinLoop() that checks whether a given Linked List contains a loop or not and if the loop is present then return the count of nodes in a loop or else return 0. C is the position of the node to which the last node is connected. If it is 0 then no loop.
+
+
+
+Example 1:
+
+Input:
+N = 10
+value[]={25,14,19,33,10,21,39,90,58,45}
+C = 4
+Output: 7
+Explanation: The loop is 45->33. So
+length of loop is 33->10->21->39->
+90->58->45 = 7. The number 33 is
+connected to the last node to form the
+loop because according to the input the
+4th node from the beginning(1 based
+index) will be connected to the last
+node for the loop.
+Example 2:
+
+Input:
+N = 2
+value[] = {1,0}
+C = 1
+Output: 2
+Explanation: The length of the loop
+is 2.
+Your Task:
+The task is to complete the function countNodesinLoop() which contains the only argument as reference to head of linked list and return the lenght of the loop ( 0 if there is no loop).
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= N <= 500
+0 <= C <= N-1
+
+
+
+/*
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+//Function should return the length of the loop in LL.
+
+class Solution
+{
+    //Function to find the length of a loop in the linked list.
+    static int countNodesinLoop(Node head)
+    {
+        //Add your code here.
+        if (head == null) {
+            return 0;
+        }
+        int cycleLength = 0;
+        Node slow = head; // moves 1 step ahead
+        Node fast = head; // moves 2 step ahed
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = fast.next;
+                cycleLength = 1;
+                while (slow != fast) {
+                    cycleLength++;
+                    slow = slow.next;
+                }
+                return cycleLength;
+            }
+        }
+        return 0;
+    }
+}
