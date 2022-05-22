1d152a3b9053f191391f53a2caf62698583be1e6
Rohithv07 <rohithv63@gmail.com>
2022-05-22 09:46:13 +0530

    Leetcode day 22 probelm in java

---
+++ MayDailyChallenge2022/22_Palindromic_Substrings.java
@@ -0,0 +1,53 @@
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+
+
+class Solution {
+    public int countSubstrings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        if (s.length() == 1) {
+            return 1;
+        }
+        int length = s.length();
+        int count = 0;
+        for (int start = 0; start < length; start++) {
+            count += expand(s, start, start);
+            count += expand(s, start, start + 1);
+        }
+        return count;
+    }
+    
+    private int expand(String s, int start, int end) {
+        int count = 0;
+        while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            count++;
+            start--;
+            end++;
+        }
+        return count;
+    }
+}
