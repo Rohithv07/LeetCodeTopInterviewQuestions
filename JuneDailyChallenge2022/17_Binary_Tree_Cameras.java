cedf8a99b3b46ff31a0081951d4e8f986bf2966e
Rohithv07 <rohithv63@gmail.com>
2022-06-17 09:56:53 +0530

    Leetcode day 17 problem in java

---
+++ JuneDailyChallenge2022/17_Binary_Tree_Cameras.java
@@ -0,0 +1,60 @@
+You are given the root of a binary tree. We install cameras on the tree nodes where each camera at a node can monitor its parent, itself, and its immediate children.
+
+Return the minimum number of cameras needed to monitor all nodes of the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [0,0,null,0,0]
+Output: 1
+Explanation: One camera is enough to monitor all nodes if placed as shown.
+Example 2:
+
+
+Input: root = [0,0,null,0,null,0,null,null,0]
+Output: 2
+Explanation: At least two cameras are needed to monitor all nodes of the tree. The above image shows one of the valid configurations of camera placement.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+Node.val == 0
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int result = 0;
+    public int minCameraCover(TreeNode root) {
+        return (dfs(root) < 1 ? 1 : 0) + result;
+    }
+    
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 2;
+        }
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        if (left == 0 || right == 0) {
+            result++;
+            return 1;
+        }
+        return left == 1 || right == 1 ? 2 : 0;
+    }
+}
