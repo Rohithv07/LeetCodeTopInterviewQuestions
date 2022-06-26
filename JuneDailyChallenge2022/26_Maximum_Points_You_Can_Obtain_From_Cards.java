d8c993c3d9125bf26e14fc318ac65b1374ad2e39
Rohithv07 <rohithv63@gmail.com>
2022-06-26 17:25:17 +0530

    Leetcode day 20 problem in java

---
+++ JuneDailyChallenge2022/26_Maximum_Points_You_Can_Obtain_From_Cards.java
@@ -0,0 +1,58 @@
+There are several cards arranged in a row, and each card has an associated number of points. The points are given in the integer array cardPoints.
+
+In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+
+Your score is the sum of the points of the cards you have taken.
+
+Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+
+ 
+
+Example 1:
+
+Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+Output: 12
+Explanation: After the first step, your score will always be 1. However, choosing the rightmost card first will maximize your total score. The optimal strategy is to take the three cards on the right, giving a final score of 1 + 6 + 5 = 12.
+Example 2:
+
+Input: cardPoints = [2,2,2], k = 2
+Output: 4
+Explanation: Regardless of which two cards you take, your score will always be 4.
+Example 3:
+
+Input: cardPoints = [9,7,7,9,7,7,9], k = 7
+Output: 55
+Explanation: You have to take all the cards. Your score is the sum of points of all cards.
+ 
+
+Constraints:
+
+1 <= cardPoints.length <= 105
+1 <= cardPoints[i] <= 104
+1 <= k <= cardPoints.length
+
+
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int length = cardPoints.length;
+        if (k == length) {
+            int totalSum = 0;
+            for (int card : cardPoints) {
+                totalSum += card;
+            }
+            return totalSum;
+        }
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 0; i < k; i++) {
+            leftSum += cardPoints[i];
+        }
+        int result = leftSum;
+        for (int i = 0; i < k; i++) {
+            leftSum -= cardPoints[k - i - 1];
+            rightSum += cardPoints[length - i - 1];
+            result = Math.max(result, leftSum + rightSum);
+        }
+        return result;
+    }
+}
