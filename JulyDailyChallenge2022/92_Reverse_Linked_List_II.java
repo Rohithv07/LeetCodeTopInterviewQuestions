16005d9677ae8c139f23500913f2535252451110
Rohithv07 <rohithv63@gmail.com>
2022-07-21 09:52:34 +0530

    Leetcode day 92 problem in java

---
+++ JulyDailyChallenge2022/92_Reverse_Linked_List_II.java
@@ -0,0 +1,69 @@
+Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [5], left = 1, right = 1
+Output: [5]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= n <= 500
+-500 <= Node.val <= 500
+1 <= left <= right <= n
+ 
+
+Follow up: Could you do it in one pass?
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode previous = null;
+        ListNode current = head;
+        while (left > 1) {
+            previous = current;
+            current = current.next;
+            left--;
+            right--;
+        }
+        ListNode connection = previous;
+        ListNode tail = current;
+        while (right > 0) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+            right--;
+        }
+        if (connection != null) {
+            connection.next = previous;
+        }
+        else {
+            head = previous;
+        };
+        tail.next = current;
+        return head;
+        
+    }
+}
