306e74387c9379abb6d097ef8e4e553de376d810
Rohithv07 <rohithv63@gmail.com>
2022-07-24 10:36:28 +0530

    Leetcode day 24 problem in java

---
+++ JulyDailyChallenge2022/24_Search_A_2d_Matrix_II.java
@@ -0,0 +1,52 @@
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-109 <= matrix[i][j] <= 109
+All the integers in each row are sorted in ascending order.
+All the integers in each column are sorted in ascending order.
+-109 <= target <= 109
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            // if currentcell value greater than target, column can be skipped
+            if (matrix[row][col] > target) {
+                col--;
+            }
+            // if currentcell value lesser than target, then row can be skipped
+            else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
