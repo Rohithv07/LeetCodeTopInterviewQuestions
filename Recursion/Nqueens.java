44e9a721c73942ed6c9885eab99f31154759c6a1
Rohithv07 <rohithv63@gmail.com>
2022-08-09 23:51:53 +0530

    Recursion series in java

--- Recursion/Nqueens.java
+++ Recursion/Nqueens.java
@@ -68,3 +68,55 @@ class Solution {
     }
     
 }
+
+
+// optimised way where we can use a kind of hashing technique to get rid of canPlace method
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char [][] chessBoard = new char [n][n];
+        /* these 3 array can be used as a hash to know whether a queen has been placed or not
+        */
+        int [] leftRow = new int [n];
+        int [] leftUpper = new int [2 * n - 1];
+        int [] leftDown = new int [2 * n - 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                chessBoard[i][j] = '.';
+            }
+        }
+        backtrack(result, chessBoard, 0, n, leftRow, leftUpper, leftDown);
+        return result;
+    }
+    
+    private void backtrack(List<List<String>> result, char [][] chessBoard, int col, int n, int [] leftRow, int [] leftUpper, int [] leftDown) {
+        if (col == n) {
+            List<String> currentConfig = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                String current = new String(chessBoard[i]);
+                currentConfig.add(current);
+            }
+            result.add(new ArrayList<>(currentConfig));
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (leftRow[i] == 1 || leftUpper[n - 1 + col - i] == 1 || leftDown[i + col] == 1) {
+                continue;
+            }
+            // if (!canPlace(chessBoard, i, col, n)) {
+            //     continue;
+            // }
+            chessBoard[i][col] = 'Q';
+            leftRow[i] = 1;
+            leftUpper[n - 1 + col - i] = 1;
+            leftDown[i + col] = 1;
+            backtrack(result, chessBoard, col + 1, n, leftRow, leftUpper, leftDown);
+            chessBoard[i][col] = '.';
+            leftRow[i] = 0;
+            leftUpper[n - 1 + col - i] = 0;
+            leftDown[i + col] = 0;
+        }
+    }
+    
+}
