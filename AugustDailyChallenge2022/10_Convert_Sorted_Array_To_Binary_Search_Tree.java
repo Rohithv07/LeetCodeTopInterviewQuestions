d9c0e9b101c147706f3b4f29f40d3db5689ea9db
Rohithv07 <rohithv63@gmail.com>
2022-08-10 09:38:45 +0530

    Leetcode day 10 problem in java

---
+++ AugustDailyChallenge2022/10_Convert_Sorted_Array_To_Binary_Search_Tree.java
@@ -0,0 +1,66 @@
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+ 
+
+Example 1:
+
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in a strictly increasing order.
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        int length = nums.length;
+        int left = 0;
+        int right = length - 1;
+        return bstConstruction(nums, left, right);
+    }
+    
+    private TreeNode bstConstruction(int [] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int middle = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[middle]);
+        root.left = bstConstruction(nums, left, middle - 1);
+        root.right = bstConstruction(nums, middle + 1, right);
+        return root;
+    }
+}
