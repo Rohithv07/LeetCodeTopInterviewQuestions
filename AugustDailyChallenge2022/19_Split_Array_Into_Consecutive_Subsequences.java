e48ccf25df4060b257cd8dd8fd065ab6ab35754d
Rohithv07 <rohithv63@gmail.com>
2022-08-19 19:38:15 +0530

    Leetcode day 19 problem in java

---
+++ AugustDailyChallenge2022/19_Split_Array_Into_Consecutive_Subsequences.java
@@ -0,0 +1,61 @@
+You are given an integer array nums that is sorted in non-decreasing order.
+
+Determine if it is possible to split nums into one or more subsequences such that both of the following conditions are true:
+
+Each subsequence is a consecutive increasing sequence (i.e. each integer is exactly one more than the previous integer).
+All subsequences have a length of 3 or more.
+Return true if you can split nums according to the above conditions, or false otherwise.
+
+A subsequence of an array is a new array that is formed from the original array by deleting some (can be none) of the elements without disturbing the relative positions of the remaining elements. (i.e., [1,3,5] is a subsequence of [1,2,3,4,5] while [1,3,2] is not).
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,3,4,5]
+Output: true
+Explanation: nums can be split into the following subsequences:
+[1,2,3,3,4,5] --> 1, 2, 3
+[1,2,3,3,4,5] --> 3, 4, 5
+Example 2:
+
+Input: nums = [1,2,3,3,4,4,5,5]
+Output: true
+Explanation: nums can be split into the following subsequences:
+[1,2,3,3,4,4,5,5] --> 1, 2, 3, 4, 5
+[1,2,3,3,4,4,5,5] --> 3, 4, 5
+Example 3:
+
+Input: nums = [1,2,3,4,4,5]
+Output: false
+Explanation: It is impossible to split nums into consecutive increasing subsequences of length 3 or more.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-1000 <= nums[i] <= 1000
+nums is sorted in non-decreasing order.
+
+
+class Solution {
+   public boolean isPossible(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>(), appendfreq = new HashMap<>();
+        for (int i : nums) freq.put(i, freq.getOrDefault(i,0) + 1);
+        for (int i : nums) {
+            if (freq.get(i) == 0) continue;
+            else if (appendfreq.getOrDefault(i,0) > 0) {
+                appendfreq.put(i, appendfreq.get(i) - 1);
+                appendfreq.put(i+1, appendfreq.getOrDefault(i+1,0) + 1);
+            }   
+            else if (freq.getOrDefault(i+1,0) > 0 && freq.getOrDefault(i+2,0) > 0) {
+                freq.put(i+1, freq.get(i+1) - 1);
+                freq.put(i+2, freq.get(i+2) - 1);
+                appendfreq.put(i+3, appendfreq.getOrDefault(i+3,0) + 1);
+            }
+            else return false;
+            freq.put(i, freq.get(i) - 1);
+        }
+        return true;
+    }
+}
