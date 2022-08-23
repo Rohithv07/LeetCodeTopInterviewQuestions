73bea3e4a89e461752a85ab2648eaaeb037ecd19
Rohithv07 <rohithv63@gmail.com>
2022-08-23 22:10:26 +0530

    Codeforces 199A problem in java

---
+++ CodeForces/Codeforces/src/problemsolving2/HexadecimalTheorem.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package problemsolving2;
+
+import java.util.Scanner;
+
+/**
+ * @author rohithvazhathody
+ *
+ *
+ */
+public class HexadecimalTheorem {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		System.out.println("0 0 " + n);
+	}
+}
