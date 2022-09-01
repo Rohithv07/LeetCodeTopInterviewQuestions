2afa8786b3c14a08edcd236c8ac146716699cd6d
Rohithv07 <rohithv63@gmail.com>
2022-09-01 22:15:35 +0530

    Codeforces 1216B problem in java

---
+++ CodeForces/Codeforces/src/problemsolving2/Shooting.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package problemsolving2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author rohithvazhathody
+ *
+ *
+ */
+public class Shooting {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Pair[] nums = new Pair[n];
+		for (int i = 0; i < n; i++) {
+			int value = sc.nextInt();
+			nums[i] = new Pair(value, i);
+		}
+		Arrays.sort(nums, (p1, p2) -> Integer.compare(p2.value, p1.value));
+		int[] result = new int[n];
+		result[0] = nums[0].index + 1;
+		int total = 1;
+		int can = 1;
+		for (int i = 1; i < n; i++) {
+			total = total + nums[i].value * can + 1;
+			result[i] = nums[i].index + 1;
+			can++;
+		}
+		System.out.println(total);
+		for (int num : result) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+	}
+}
+
+class Pair {
+	int value;
+	int index;
+
+	public Pair(int value, int index) {
+		this.value = value;
+		this.index = index;
+	}
+}
