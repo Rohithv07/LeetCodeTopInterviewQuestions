29874be00f1920f2d6dac4d04aba3060e3959c33
Rohithv07 <rohithv63@gmail.com>
2022-09-02 11:23:18 +0530

    Leetcode day 2 problem in java

---
+++ SeptemberDailyChallenge2022/02_Average_Of_Levels_In_Binary_Tree.java
@@ -0,0 +1,66 @@
+Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [3.00000,14.50000,11.00000]
+Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
+Hence return [3, 14.5, 11].
+Example 2:
+
+
+Input: root = [3,9,20,15,7]
+Output: [3.00000,14.50000,11.00000]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0.0;
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                sum += (double) current.val;
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+            double average = (sum / (double)size);
+            result.add(average);
+        }
+        return result;
+    }
+}
