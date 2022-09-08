bafd3b260c8bf23acacc27b285e280961ada719e
Rohithv07 <rohithv63@gmail.com>
2022-09-08 10:34:49 +0530

    Leetcode day 8 problem in java

---
+++ SeptemberDailyChallenge2022/08_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,106 @@
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        inorder(root, result);
+        return result;
+    }
+    
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+    }
+}
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (true) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            else {
+                if (stack.isEmpty()) {
+                    break;
+                }
+                current = stack.pop();
+                result.add(current.val);
+                current = current.right;
+            }
+        }
+        return result;
+    }
+}
