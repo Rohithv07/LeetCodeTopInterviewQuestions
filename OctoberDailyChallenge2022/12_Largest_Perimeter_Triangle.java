3afb5190affbe8a2f91a13ea6cf0fd73626f0196
Rohithv07 <rohithv63@gmail.com>
2022-10-12 22:37:55 +0530

    Leetcode day 12 problem in java

---
+++ OctoberDailyChallenge2022/12_Largest_Perimeter_Triangle.java
@@ -0,0 +1,29 @@
+Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+ 
+
+Example 1:
+
+Input: nums = [2,1,2]
+Output: 5
+Example 2:
+
+Input: nums = [1,2,1]
+Output: 0
+ 
+
+Constraints:
+
+3 <= nums.length <= 104
+1 <= nums[i] <= 106
+
+
+class Solution {
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        for (int i = A.length - 3; i >= 0; --i)
+            if (A[i] + A[i+1] > A[i+2])
+                return A[i] + A[i+1] + A[i+2];
+        return 0;
+    }
+}
