6c8876c262ac518644be3315098554386a457dc7
Rohithv07 <rohithv63@gmail.com>
2022-10-20 01:01:41 +0530

    GfG problem asked in Goldman in java copied

--- Codesignal and Geekforgeeks Combined/NumberFormation.java
+++ Codesignal and Geekforgeeks Combined/NumberFormation.java
@@ -31,3 +31,50 @@ class Solution {
         return (int)answer;
     }
 }
+
+
+class Solution {
+
+ 
+
+    public int getSum(int X, int Y, int Z) {
+        int mod = 1000000007;
+        // exactsum[i][j][k] stores the sum of
+        // all the numbers having exact
+        // i 4's, j 5's and k 6's
+        long exactsum[][][] = new long[X + 1][Y+ 1][Z + 1];
+        // exactnum[i][j][k] stores numbers
+        // of numbers having exact
+        // i 4's, j 5's and k 6's
+        long exactnum[][][] = new long[X + 1][Y + 1][Z + 1];
+        long ans = 0L;
+        exactnum[0][0][0] = 1L;
+        for (int i = 0; i <= X; ++i) {
+            for (int j = 0; j <= Y; ++j) {
+                for (int k = 0; k <= Z; ++k) {
+                    if (i > 0) {
+                        exactsum[i][j][k] += (exactsum[i - 1][j][k] * 10 +
+                                              4 * exactnum[i - 1][j][k]) %
+                                             mod;
+                        exactnum[i][j][k] += exactnum[i - 1][j][k] % mod;
+                    }
+                    if (j > 0) {
+                        exactsum[i][j][k] += (exactsum[i][j - 1][k] * 10 +
+                                              5 * exactnum[i][j - 1][k]) %
+                                             mod;
+                        exactnum[i][j][k] += exactnum[i][j - 1][k] % mod;
+                    }
+                    if (k > 0) {
+                        exactsum[i][j][k] += (exactsum[i][j][k - 1] * 10 +
+                                              6 * exactnum[i][j][k - 1]) %
+                                             mod;
+                        exactnum[i][j][k] += exactnum[i][j][k - 1] % mod;
+                    }
+                    ans += exactsum[i][j][k] % mod;
+                    ans %= mod;
+                }
+            }
+        }
+        return (int)ans;
+    }
+}
