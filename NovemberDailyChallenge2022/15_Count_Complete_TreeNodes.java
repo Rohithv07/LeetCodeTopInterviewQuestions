4fda3dae88d3b2a211c8c4996b877c8fdc3d320f
Rohithv07 <rohithv63@gmail.com>
2022-11-15 20:40:19 +0530

    Leetcode day 15 problem in java

---
+++ NovemberDailyChallenge2022/15_Count_Complete_TreeNodes.java
@@ -0,0 +1,76 @@
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6]
+Output: 6
+Example 2:
+
+Input: root = []
+Output: 0
+Example 3:
+
+Input: root = [1]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5 * 104].
+0 <= Node.val <= 5 * 104
+The tree is guaranteed to be complete.
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = findLeftDepth(root);
+        int rightDepth = findRightDepth(root);
+        if (leftDepth == rightDepth) {
+            return (1 << leftDepth) - 1;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    
+    private int findLeftDepth(TreeNode node) {
+        int count = 0;
+        while (node != null) {
+            count++;
+            node = node.left;
+        }
+        return count;
+    }
+    
+    private int findRightDepth(TreeNode node) {
+        int count = 0;
+        while (node != null) {
+            count++;
+            node = node.right;
+        }
+        return count;
+    }
+}
