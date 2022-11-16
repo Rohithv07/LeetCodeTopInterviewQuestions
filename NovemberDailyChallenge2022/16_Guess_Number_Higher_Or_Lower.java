8189a51cdc74330a167031b3326e9d4bf4472f03
Rohithv07 <rohithv63@gmail.com>
2022-11-16 23:34:22 +0530

    Leetcode day 16 problem in java

---
+++ NovemberDailyChallenge2022/16_Guess_Number_Higher_Or_Lower.java
@@ -0,0 +1,63 @@
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+ 
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+1 <= pick <= n
+
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (guess(middle) == 0) {
+                return middle;
+            }
+            if (guess(middle) == -1) {
+                high = middle - 1;
+            }
+            else {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
+}
