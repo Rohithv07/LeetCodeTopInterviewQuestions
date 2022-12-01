10e7fbe1f82d052dedbbca92cd9b13b9c19f98e7
Rohithv07 <rohithv63@gmail.com>
2022-12-01 11:30:11 +0530

    Leetcode day 1 problem in java

---
+++ DecemberDailyChallenge2022/01_Determine_If_Two_String_Halves_Are_Alike.java
@@ -0,0 +1,52 @@
+You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+
+Return true if a and b are alike. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+Example 2:
+
+Input: s = "textbook"
+Output: false
+Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+Notice that the vowel o is counted twice.
+ 
+
+Constraints:
+
+2 <= s.length <= 1000
+s.length is even.
+s consists of uppercase and lowercase letters.
+
+
+
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int length = s.length();
+        int vowelCount = 0;
+        int middle = length / 2;
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (i < middle) {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                   ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                    vowelCount++;
+                }
+            }
+            else {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                   ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                    vowelCount--;
+                }
+            }
+        }
+        return vowelCount == 0;
+    }
+}
