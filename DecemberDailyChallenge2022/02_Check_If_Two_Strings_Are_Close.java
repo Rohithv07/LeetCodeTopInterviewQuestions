bbf32a8354dd94c7c387f3ce689a771e6aa60a09
Rohithv07 <rohithv63@gmail.com>
2022-12-02 19:43:43 +0530

    Leetcode day 2 problem in java

---
+++ DecemberDailyChallenge2022/02_Check_If_Two_Strings_Are_Close.java
@@ -0,0 +1,75 @@
+Two strings are considered close if you can attain one from the other using the following operations:
+
+Operation 1: Swap any two existing characters.
+For example, abcde -> aecdb
+Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+You can use the operations on either string as many times as necessary.
+
+Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "bca"
+Output: true
+Explanation: You can attain word2 from word1 in 2 operations.
+Apply Operation 1: "abc" -> "acb"
+Apply Operation 1: "acb" -> "bca"
+Example 2:
+
+Input: word1 = "a", word2 = "aa"
+Output: false
+Explanation: It is impossible to attain word2 from word1, or vice versa, in any number of operations.
+Example 3:
+
+Input: word1 = "cabbba", word2 = "abbccc"
+Output: true
+Explanation: You can attain word2 from word1 in 3 operations.
+Apply Operation 1: "cabbba" -> "caabbb"
+Apply Operation 2: "caabbb" -> "baaccc"
+Apply Operation 2: "baaccc" -> "abbccc"
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 105
+word1 and word2 contain only lowercase English letters.
+
+
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int length1 = word1.length();
+        int length2 = word2.length();
+        if (length1 != length2) {
+            return false;
+        }
+        int [] count1 = new int [26];
+        int [] count2 = new int [26];
+        for (int i = 0; i < length1; i++) {
+            char ch1 = word1.charAt(i);
+            char ch2 = word2.charAt(i);
+            count1[ch1 - 'a']++;
+            count2[ch2 - 'a']++;
+        }
+        int pos1 = 0;
+        int pos2 = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] == 0 && count2[i] != 0) {
+                return false;
+            }
+            if (count2[i] == 0 && count1[i] != 0) {
+                return false;
+            }
+        }
+        Arrays.sort(count1);
+        Arrays.sort(count2);
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] != count2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
