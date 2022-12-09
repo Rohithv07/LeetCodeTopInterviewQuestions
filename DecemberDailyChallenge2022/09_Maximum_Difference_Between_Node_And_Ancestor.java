c4acbc1661a2c9fb82a7d882e15f66dff1592750
Rohithv07 <rohithv63@gmail.com>
2022-12-10 00:44:55 +0530

    Leetcode day 9 problem in java

---
+++ DecemberDailyChallenge2022/09_Maximum_Difference_Between_Node_And_Ancestor.java
@@ -0,0 +1,63 @@
+Given the root of a binary tree, find the maximum value v for which there exist different nodes a and b where v = |a.val - b.val| and a is an ancestor of b.
+
+A node a is an ancestor of b if either: any child of a is equal to b or any child of a is an ancestor of b.
+
+ 
+
+Example 1:
+
+
+Input: root = [8,3,10,1,6,null,14,null,null,4,7,13]
+Output: 7
+Explanation: We have various ancestor-node differences, some of which are given below :
+|8 - 3| = 5
+|3 - 7| = 4
+|8 - 1| = 7
+|10 - 13| = 3
+Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+Example 2:
+
+
+Input: root = [1,null,2,null,0,3]
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 5000].
+0 <= Node.val <= 105
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxAncestorDiff(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return postorder(root, root.val, root.val);
+    }
+
+    private int postorder(TreeNode node, int currentMax, int currentMin) {
+        if (node == null) {
+            return currentMax - currentMin;
+        }
+        currentMax = Math.max(currentMax, node.val);
+        currentMin = Math.min(currentMin, node.val);
+        return Math.max(postorder(node.left, currentMax, currentMin),
+        postorder(node.right, currentMax, currentMin));
+    }
+}
