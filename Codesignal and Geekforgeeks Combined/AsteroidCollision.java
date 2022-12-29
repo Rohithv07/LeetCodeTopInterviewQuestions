c8be5b691c3ad2eb1816c0dc646b0b5559d856b0
Rohithv07 <rohithv63@gmail.com>
2022-12-29 17:29:21 +0530

    GfG problem asked in interviews in java

--- Codesignal and Geekforgeeks Combined/AsteroidCollision.java
+++ Codesignal and Geekforgeeks Combined/AsteroidCollision.java
@@ -98,3 +98,34 @@ class Solution {
         return result;
     }
 }
+
+
+// more understandable code from geeksforgeeks
+
+
+// Back-end complete function Template for Java
+
+class Solution {
+    public static int[] asteroidCollision(int N, int[] asteroids) {
+        Stack<Integer> s = new Stack<>();
+        for (int i : asteroids) {
+            if (i > 0) {
+                s.push(i);
+            } else { // i is negative
+                while (!s.isEmpty() && s.peek() > 0 && s.peek() < Math.abs(i)) {
+                    s.pop();
+                }
+                if (s.isEmpty() || s.peek() < 0) {
+                    s.push(i);
+                } else if (i + s.peek() == 0) {
+                    s.pop(); // equal
+                }
+            }
+        }
+        int[] res = new int[s.size()];
+        for (int i = res.length - 1; i >= 0; i--) {
+            res[i] = s.pop();
+        }
+        return res;
+    }
+}
