67ab52c05d7ce0bbe5ba4241be0a1da3450191b1
Rohithv07 <rohithv63@gmail.com>
2022-12-30 10:39:28 +0530

    Leetcode day 30 problem in java

---
+++ DecemberDailyChallenge2022/30_All_Path_From_Source_To_Target.java
@@ -0,0 +1,55 @@
+Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+
+The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+
+ 
+
+Example 1:
+
+
+Input: graph = [[1,2],[3],[3],[]]
+Output: [[0,1,3],[0,2,3]]
+Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+Example 2:
+
+
+Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
+Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+ 
+
+Constraints:
+
+n == graph.length
+2 <= n <= 15
+0 <= graph[i][j] < n
+graph[i][j] != i (i.e., there will be no self-loops).
+All the elements of graph[i] are unique.
+The input graph is guaranteed to be a DAG.
+
+
+
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        int length = graph.length;
+        if (length == 0 || graph == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        storeAllPath(result, current, length - 1, 0, graph);
+        return result;
+    }
+
+    private void storeAllPath(List<List<Integer>> result, List<Integer> current, int destination, int moveNode, int [][] graph) {
+        current.add(moveNode);
+        if (moveNode == destination) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        int[] children = graph[moveNode];
+        for (int child : children) {
+            storeAllPath(result, current, destination, child, graph);
+            current.remove(current.size() - 1);
+        }
+    }
+}
