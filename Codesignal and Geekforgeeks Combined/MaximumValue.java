7afb740ef31b2a7b6c53f00ac8e23a89706e171c
Rohithv07 <rohithv63@gmail.com>
2023-01-02 17:06:14 +0530

    GfG problem asked in amazon in java

--- Codesignal and Geekforgeeks Combined/MaximumValue.java
+++ Codesignal and Geekforgeeks Combined/MaximumValue.java
@@ -49,3 +49,67 @@ class Solution {
     }
 }
 
+
+/*
+Tree node structure  used in the program
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    ArrayList<Integer> maximumValue(Node node) {
+       //code here
+       ArrayList<Integer> result = new ArrayList<>();
+       if (node == null) {
+           return result;
+       }
+       //bfs(node, result);
+        dfs(node, result, 0);
+       return result;
+    }
+    
+    private void dfs(Node node, ArrayList<Integer> result, int level) {
+        if (node == null) {
+            return;
+        }
+        if (result.size() == level) {
+            result.add(node.data);
+        }
+        if (result.size() > level) {
+            int currentMax = result.get(level);
+            int max = Math.max(currentMax, node.data);
+            result.set(level, max); 
+        }
+        dfs(node.left, result, level + 1);
+        dfs(node.right, result, level + 1);
+    }
+    
+    private void bfs(Node node, ArrayList<Integer> result) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int currentLevelMax = 0;
+            for (int i = 0; i < size; i++) {
+                Node topNode = queue.poll();
+                currentLevelMax = Math.max(currentLevelMax, topNode.data);
+                if (topNode.left != null) {
+                    queue.offer(topNode.left);
+                }
+                if (topNode.right != null) {
+                    queue.offer(topNode.right);
+                }
+            }
+            result.add(currentLevelMax);
+        }
+    }
+}
+
