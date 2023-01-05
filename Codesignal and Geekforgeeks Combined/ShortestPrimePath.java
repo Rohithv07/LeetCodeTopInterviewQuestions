8dd69035bce1ffacdcf5b63083fe57e5d9fc94c8
Rohithv07 <rohithv63@gmail.com>
2023-01-06 00:35:24 +0530

    GfG problem asked in adobe in java

--- Codesignal and Geekforgeeks Combined/ShortestPrimePath.java
+++ Codesignal and Geekforgeeks Combined/ShortestPrimePath.java
@@ -114,3 +114,69 @@ class Solution
         return graph;
     }
 }
+
+
+// without using strings
+
+class Solution
+{
+    int[] prime;
+    final int MAX = 9999;
+    Solution()
+    {
+        // Every index of prime stores zero or one
+        // If prime[i] is zero means i is not a prime
+        // If prime[i] is one means i is a prime
+        prime = new int [MAX + 1];
+        Arrays.fill(prime, 1);
+        prime[0] = 0;
+        prime[1] = 0;
+        for (int i = 2; i * i < MAX + 1; i++) {
+            if (prime[i] == 1) {
+                for (int j = i; j * i < MAX + 1; j++) {
+                    prime[i * j] = 0;
+                }
+            }
+        }
+    }
+    
+    public int shortestPath(int num1,int num2){
+        // Complete this function using prime array
+        if (num1 == num2) {
+            return 0;
+        }
+        return bfs(num1, num2);
+    }
+    
+    private int bfs(int n1, int n2) {
+        Queue<Integer> queue = new LinkedList<>();
+        int shortestDistance = 0;
+        queue.offer(n1);
+        Set<Integer> visited = new HashSet<>();
+        visited.add(n1);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int top = queue.poll();
+                if (top == n2) {
+                    return shortestDistance;
+                }
+                // we have 4 digit numbers only
+                for (int digit = 0; digit < 4; digit++) {
+                    for (int num = 0; num < 10; num++) {
+                        int nextNumber = top;
+                        nextNumber -= (nextNumber / (int)(Math.pow(10, digit))) % 10 * (int)(Math.pow(10, digit));
+                        nextNumber += num * (int)(Math.pow(10, digit));
+                        if (nextNumber >= 1000 && nextNumber <= 9999 && !visited.contains(nextNumber)
+                        && prime[nextNumber] == 1) {
+                            queue.offer(nextNumber);
+                            visited.add(nextNumber);
+                        }
+                    }
+                }
+            }
+            shortestDistance++;
+        }
+        return -1;
+    }
+}
