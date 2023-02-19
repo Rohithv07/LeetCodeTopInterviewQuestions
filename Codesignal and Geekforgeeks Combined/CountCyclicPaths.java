db8b43d73a7180737462552aac29b537dfbb72d4
Rohithv07 <rohithv63@gmail.com>
2023-02-20 00:21:10 +0530

    GfG problem asked in oyo rooms in java

--- Codesignal and Geekforgeeks Combined/CountCyclicPaths.java
+++ Codesignal and Geekforgeeks Combined/CountCyclicPaths.java
@@ -1,3 +1,35 @@
+Given a triangular pyramid with its vertices marked as O, A, B and C and a number N, the task is to find the number of ways such that a person starting from the origin O initially, reaches back to the origin in N steps. In a single step, a person can go to any of its adjacent vertices.
+
+Lightbox
+
+
+Example 1:
+
+Input:
+N = 1
+Output: 0
+Explanation: The minimum length of
+a cyclic path is 2.
+Example 2:
+
+Input:
+N = 2
+Output: 3
+Explanation: The three paths are :
+O-A-O, O-B-O, O-C-O
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function countPaths() which takes an integer N as input parameter and returns the number of possible paths. Since the answer may be big, return it modulo (10^9+7). 
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 106
+
+
 class Solution{
 	public static int countPaths(int n){
 		//code here
@@ -24,3 +56,38 @@ class Solution{
 		return (int)(dp[0][n] % mod);
 	}
 }
+
+
+
+/*
+dp[origin][i] = 3*dp[lower][i-1]   (We can move to the origin from 3 lower vertices)
+
+dp[lower][i] = 2*dp[lower][i-1] + dp[origin][i-1] 
+(We can move to a lower vertice from either one of two remaining lower
+vertices or the origin)
+
+*/
+
+
+class Solution{
+	public static int countPaths(int n){
+		//code here
+		if (n == 1) {
+		    return 0;
+		}
+		
+		long low = 1l;
+		long  high = 0l;
+		long tlow = 1l;
+		long thigh = 0l;
+		final long MOD = (long)(1e9 + 7);
+		for (int i = 2; i <= n; i++) {
+		    high = (3 * tlow) % MOD;
+		    low = (((2 * tlow) % MOD) + thigh) % MOD;
+		    tlow = low;
+		    thigh = high;
+		}
+		return (int)high;
+	}
+}
+// 0, 3, 6, 21
