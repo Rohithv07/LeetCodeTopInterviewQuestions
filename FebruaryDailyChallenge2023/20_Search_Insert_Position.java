36a4b1fbc385f0e4dbecef0d3175f7d230d46045
Rohithv07 <rohithv63@gmail.com>
2023-02-20 19:14:20 +0530

    Leetcode day 20 problem in java

---
+++ FebruaryDailyChallenge2023/20_Search_Insert_Position.java
@@ -0,0 +1,51 @@
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int length = nums.length;
+        int low = 0;
+        int high = length - 1;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            }
+            if (nums[middle] > target) {
+                high = middle - 1;
+            }
+            else {
+                low = middle + 1;
+            }
+        }
+        return low;
+    }
+}
