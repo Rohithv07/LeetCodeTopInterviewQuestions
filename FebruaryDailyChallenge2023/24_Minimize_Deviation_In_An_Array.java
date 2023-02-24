35d40173056b79dd0a7089e58621bd8e49eae7aa
Rohithv07 <rohithv63@gmail.com>
2023-02-24 22:07:06 +0530

    Leetcode day 24 problem in java

---
+++ FebruaryDailyChallenge2023/24_Minimize_Deviation_In_An_Array.java
@@ -0,0 +1,67 @@
+You are given an array nums of n positive integers.
+
+You can perform two types of operations on any element of the array any number of times:
+
+If the element is even, divide it by 2.
+For example, if the array is [1,2,3,4], then you can do this operation on the last element, and the array will be [1,2,3,2].
+If the element is odd, multiply it by 2.
+For example, if the array is [1,2,3,4], then you can do this operation on the first element, and the array will be [2,2,3,4].
+The deviation of the array is the maximum difference between any two elements in the array.
+
+Return the minimum deviation the array can have after performing some number of operations.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: 1
+Explanation: You can transform the array to [1,2,3,2], then to [2,2,3,2], then the deviation will be 3 - 2 = 1.
+Example 2:
+
+Input: nums = [4,1,5,20,3]
+Output: 3
+Explanation: You can transform the array after two operations to [4,2,5,5,3], then the deviation will be 5 - 2 = 3.
+Example 3:
+
+Input: nums = [2,10,8]
+Output: 3
+ 
+
+Constraints:
+
+n == nums.length
+2 <= n <= 5 * 104
+1 <= nums[i] <= 109
+
+
+class Solution {
+    public int minimumDeviation(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        TreeSet<Integer> set = new TreeSet<>();
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < length; i++) {
+            if (nums[i] % 2 == 0) {
+                set.add(nums[i]);
+            }
+            else {
+                set.add(nums[i] * 2);
+            }
+        }
+        while (true) {
+            int lastValue = set.last();
+            answer = Math.min(answer, lastValue - set.first());
+            if (lastValue % 2 == 0) {
+                set.remove(lastValue);
+                set.add(lastValue / 2);
+            }
+            else {
+                break;
+            }
+        }
+        return answer;
+    }
+}
