470a9a8d2c995967564516afe6500af000cf640e
Rohithv07 <rohithv63@gmail.com>
2023-03-11 21:35:06 +0530

    Leetcode day 11 problem in java

---
+++ MarchDailyChallenge2023/11_Convert_Sorted_List_To_BST.java
@@ -0,0 +1,70 @@
+Given the head of a singly linked list where elements are sorted in ascending order, convert it to a 
+height-balanced
+ binary search tree.
+
+ 
+
+Example 1:
+
+
+Input: head = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+Example 2:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in head is in the range [0, 2 * 104].
+-105 <= Node.val <= 105
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null)
+            return null;
+        if (head.next == null)
+            return new TreeNode(head.val);
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode previousSlow = null;
+        while (fast != null && fast.next != null) {
+            previousSlow = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        previousSlow.next = null;
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }
+}
