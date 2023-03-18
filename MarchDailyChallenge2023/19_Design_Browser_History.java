520b7d40cedb5f9802050c02e4ee85c2677be569
Rohithv07 <rohithv63@gmail.com>
2023-03-18 11:42:42 +0530

    Leetcode day 18 problem in java

---
+++ MarchDailyChallenge2023/19_Design_Browser_History.java
@@ -0,0 +1,81 @@
+You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
+
+Implement the BrowserHistory class:
+
+BrowserHistory(string homepage) Initializes the object with the homepage of the browser.
+void visit(string url) Visits url from the current page. It clears up all the forward history.
+string back(int steps) Move steps back in history. If you can only return x steps in the history and steps > x, you will return only x steps. Return the current url after moving back in history at most steps.
+string forward(int steps) Move steps forward in history. If you can only forward x steps in the history and steps > x, you will forward only x steps. Return the current url after forwarding in history at most steps.
+ 
+
+Example:
+
+Input:
+["BrowserHistory","visit","visit","visit","back","back","forward","visit","forward","back","back"]
+[["leetcode.com"],["google.com"],["facebook.com"],["youtube.com"],[1],[1],[1],["linkedin.com"],[2],[2],[7]]
+Output:
+[null,null,null,null,"facebook.com","google.com","facebook.com",null,"linkedin.com","google.com","leetcode.com"]
+
+Explanation:
+BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+browserHistory.visit("google.com");       // You are in "leetcode.com". Visit "google.com"
+browserHistory.visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
+browserHistory.visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
+browserHistory.back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+browserHistory.back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
+browserHistory.forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+browserHistory.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
+browserHistory.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
+browserHistory.back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+browserHistory.back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
+ 
+
+Constraints:
+
+1 <= homepage.length <= 20
+1 <= url.length <= 20
+1 <= steps <= 100
+homepage and url consist of  '.' or lower case English letters.
+At most 5000 calls will be made to visit, back, and forward.
+
+
+class BrowserHistory {
+    Stack<String> back;
+    Stack<String> forward;
+    public BrowserHistory(String homepage) {
+        back = new Stack<>();
+        forward = new Stack<>();
+        back.push(homepage);
+    }
+    
+    public void visit(String url) {
+        back.push(url);
+        forward.clear(); // if we go to new page, then we can't move forward, we can only moive backward, so pushed into back stack.
+    }
+    
+    public String back(int steps) {
+        int count = 0;
+        while (count < steps && back.size() > 1) {
+            count += 1;
+            forward.push(back.pop()); // if we move backward, then we can after sometime move forward, so fill forward stack.
+        }
+        return back.peek();
+    }
+    
+    public String forward(int steps) {
+        int count = 0;
+        while (count < steps && forward.size() > 0) {
+            count += 1;
+            back.push(forward.pop()); // same idea as that of method back. If we move forward, then after sometime we can move backward. SO we fill the back stack.
+        }
+        return back.peek();
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
